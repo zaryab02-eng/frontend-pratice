8eb172f139e474366f2bf37e17d8987061d9d0ba
Zaryab <mohdzaryab99@gmail.com>
2025-07-26 22:15:35 +0530

    Added message for if the user press sumbit or enter when input blank

--- Name-Greeter/src/script.js
+++ Name-Greeter/src/script.js
@@ -2,17 +2,22 @@ const inputName = document.getElementById("name");
 const button = document.getElementById("submit");
 const namePrint = document.getElementById("hello-area");
 
-button.addEventListener("click", function () {
+// Step 1: Put your logic in a function
+function greetUser() {
     const userName = inputName.value;
-    namePrint.innerText = `Hello ${userName}! , Have a great day ahead`;
-    inputName.value = " ";
-});
-
-  inputName.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      // Step 3: Get value
-      let userName = inputName.value;
-      namePrint.innerText = `Hello ${userName}! , Have a great day ahead`;
-      inputName.value = " ";
+    if (userName === "") {
+        namePrint.innerText = "Please enter your name first!";
+    } else {
+        namePrint.innerText = `Hello ${userName}, Have a great day ahead`;
+        inputName.value = "";
     }
-  });
+}
+
+// Step 2: Call the function from both events
+button.addEventListener("click", greetUser);
+
+inputName.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        greetUser(); // Same function!
+    }
+});
