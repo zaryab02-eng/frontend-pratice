9128d8ed478d63a53878eeb72201769b4b0461a4
Zaryab <mohdzaryab99@gmail.com>
2025-07-28 02:17:42 +0530

    Project Completed

---
+++ Password-Checker/src/script.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#input input");
+const msg = document.getElementById("message");
+const button = document.getElementById("button");
+
+
+const password = "secret123";
+
+function checkPassword () {
+     
+    const inPass = input.value.trim();
+    
+    if (inPass === "") {
+        msg.innerText = "Please enter password!";
+        return;
+    }
+
+    if(inPass == password) {
+        input.value = '';
+        msg.innerText = "✅Access Granted!";
+    } else {
+        input.value = '';
+        msg.innerText = "❌Wrong Password!";
+    }
+}
+
+button.addEventListener("click", checkPassword);
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        checkPassword();
+    }
+});
