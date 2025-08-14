ba3053bb5c943c93360f06d8de04124d304c276f
Zaryab <mohdzaryab99@gmail.com>
2025-08-14 12:29:13 +0530

    Done

--- SimpleSearchFilter/src/script.js
+++ SimpleSearchFilter/src/script.js
@@ -1,3 +1,28 @@
 const nameEnter = document.getElementById("inputName");
 const matchBtn = document.getElementById("button");
 const resultDiv = document.getElementById("result");
+
+matchBtn.addEventListener("click", () => {
+  let nameInput = nameEnter.value.trim();
+  const names = ["Arshad", "Sabiha", "Zaryab", "Altamash"];
+
+  if (nameInput === "") {
+    resultDiv.textContent = "Please Enter a name first";
+    return;
+  }
+
+  let count = 0;
+
+  names.forEach((singleName) => {
+    if (singleName.toLowerCase().includes(nameInput.toLowerCase())) {
+      count++;
+    }
+  });
+
+  if (count > 0) {
+    const word = count === 1 ? "match" : "matches";
+    resultDiv.textContent = `Found ${count} ${word}`;
+  } else {
+    resultDiv.textContent = `No ${word} Found!`;
+  }
+});
