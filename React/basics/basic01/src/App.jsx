3f11086fb1312be82d0c8f4eb9b7f40616951231
zaryab02-eng <mohdzaryab99@gmail.com>
2025-09-25 14:04:54 +0530

    set everything in react

--- React/basics/basic01/src/App.jsx
+++ React/basics/basic01/src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Hello Brother</h1>;
-}
+import React, { useState } from "react";
+
+const App = () => {
+  const [a, setA] = useState(1);
+
+  return (
+    <div>
+      <h1>The Counter is: {a}</h1>
+      <button id="incre" onClick={() => setA(a + 1)}>
+        Increment
+      </button>
+      <button id="decre" onClick={() => setA(a - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
 
 export default App;
