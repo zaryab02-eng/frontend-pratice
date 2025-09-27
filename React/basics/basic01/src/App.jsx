e94e745252f24aafccbd3f3bfd36c350fa2077cb
zaryab02-eng <mohdzaryab99@gmail.com>
2025-09-27 14:03:19 +0530

    added some deleted some

--- React/basics/basic01/src/App.jsx
+++ React/basics/basic01/src/App.jsx
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const [a, setA] = useState(0);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <>
       <div>
-        <h1>The counter is {a}</h1>
-        <div className="flex gap-2">
-          <button
-            className="bg-green-400 p-2 border-0 border-white rounded"
-            onClick={() => setA(a + 1)}
-          >
-            Increase
-          </button>
-          <button
-            className="bg-red-400 p-2 border-0 border-white rounded"
-            onClick={() => setA(a - 1)}
-          >
-            Decrease
-          </button>
-        </div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
-};
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
