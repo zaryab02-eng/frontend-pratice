23883d69776a0f26546f6fe06469b1f6571ad26c
zaryab02-eng <mohdzaryab99@gmail.com>
2025-09-30 14:17:58 +0530

    practicing

--- React/basics/basic07/src/App.jsx
+++ React/basics/basic07/src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [age, setAge] = useState(15);
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <p className="px-2.5 py-1.5 bg-blue-600 border border-white">
+          Age: {age}
+        </p>
+        <button
+          className="px-2.5 py-1.5 rounded bg-green-600 hover:bg-green-400 hover:cursor-pointer"
+          onClick={() => setAge(age + 1)}
+        >
+          Increase
+        </button>
+      </div>
+      {age < 18 && <p className="text-red-500 font-bold">You are a minor👶</p>}
+      {age >= 18 && (
+        <p className="text-green-500 font-bold">You are an adult🎓</p>
+      )}
+    </>
+  );
 };
 
 export default App;
