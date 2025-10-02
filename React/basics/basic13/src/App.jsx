ba437f94b2900ceb207a61f6c85e0cc6942caff1
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-02 13:14:33 +0530

    understanding delete button logic

--- React/basics/basic13/src/App.jsx
+++ React/basics/basic13/src/App.jsx
@@ -9,15 +9,31 @@ const App = () => {
     setNextNum(nextNum + 1);
   };
 
+  const deleteItem = (indexToDelete) => {
+    const newNum = num.filter((_, idx) => idx !== indexToDelete);
+    setNum(newNum);
+  };
+
   return (
     <>
       <div className="bg-white p-2 text-black h-32 border border-red-600 overflow-y-auto">
         {num.map((nums, idx) => (
-          <p key={idx}>{nums}</p>
+          <div key={idx} className="flex gap-2 items-center">
+            <p>{nums}</p>
+            <button
+              onClick={() => deleteItem(idx)}
+              className="p-1 bg-red-400 rounded text-white"
+            >
+              Delete
+            </button>
+          </div>
         ))}
       </div>
 
-      <button className="p-1 bg-amber-300 rounded text-black" onClick={addNum}>
+      <button
+        className="p-1 bg-amber-300 rounded text-black mt-2"
+        onClick={addNum}
+      >
         Add Next Number
       </button>
     </>
