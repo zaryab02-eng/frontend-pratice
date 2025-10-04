51b081237badb58e5d48a64f98458e51adf814a7
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-04 17:03:54 +0530

    practicing

--- React/basics/basic18/src/App.jsx
+++ React/basics/basic18/src/App.jsx
@@ -1,42 +1,36 @@
 import { useState } from "react";
 
 const App = () => {
-  const [temp, setTemp] = useState(20);
+  const [name, setName] = useState("Zaryab");
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState("Zaryab");
 
   return (
-    <>
-      <div className="flex flex-col gap-y-2">
-        <p className="px-2.5 py-2 bg-white rounded text-black">
-          Temperature {temp}°
-        </p>
-        <div className="flex gap-x-0.5">
+    <div>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
           <button
-            className="px-1.5 py-1 bg-green-600 hover:cursor-pointer hover:bg-green-400"
-            onClick={() => setTemp(temp + 1)}
+            onClick={() => {
+              setName(newName);
+              setIsEditing(false);
+            }}
+            className="bg-green-600 px-3 py-2 ml-2"
           >
-            Increase
+            Save
           </button>
-          <button
-            className="px-1.5 py-1 bg-red-600 hover:cursor-pointer hover:bg-red-400"
-            onClick={() => setTemp(temp - 1)}
-          >
-            Decrease
-          </button>
-        </div>
-        {temp < 0 && (
-          <p className="text-blue-500 font-semibold text-lg">Freezing!🥶</p>
-        )}
-        {temp >= 0 && temp < 15 && (
-          <p className="text-cyan-600 font-semibold text-lg">Cold!🧥</p>
-        )}
-        {temp >= 15 && temp < 25 && (
-          <p className="text-green-600 font-semibold text-lg">Nice!😊</p>
-        )}
-        {temp >= 25 && (
-          <p className="text-red-500 font-semibold text-lg">Hot!🔥</p>
-        )}
-      </div>
-    </>
+        </>
+      ) : (
+        <>
+          <h1>{name}</h1>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        </>
+      )}
+    </div>
   );
 };
 
