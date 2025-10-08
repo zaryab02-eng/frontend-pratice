455c7d10aeb3fca9f065d843b3174ac6b02f15bf
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-08 18:58:19 +0530

    practicing

--- React/basics/basic22/src/App.jsx
+++ React/basics/basic22/src/App.jsx
@@ -2,23 +2,42 @@ import { useState } from "react";
 
 const App = () => {
   const [showInput, setShowInput] = useState(false);
+  const [text, setText] = useState("Hello");
   return (
     <>
       <div>
         {showInput ? (
-          <input
-            type="text"
-            className="bg-white text-black px-2.5 gap-y-2 rounded"
-          />
+          <>
+            <div className="flex gap-1.5">
+              <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                className="bg-white text-black px-2.5 py-1.5 rounded"
+              />
+              <button
+                className="bg-green-700 px-2.5 py-1.5 rounded hover:bg-green-600 hover:cursor-pointer"
+                onClick={() => setShowInput(false)}
+              >
+                Save
+              </button>
+            </div>
+          </>
         ) : (
-          <p className="text-white text-2xl font-bold">Hello!</p>
+          <>
+            <div className="flex gap-1.5">
+              <p className="bg-white text-black px-2.5 py-1.5 font-bold">
+                {text}
+              </p>
+              <button
+                className="bg-blue-700 px-2.5 py-1.5 rounded hover:bg-blue-600 hover:cursor-pointer"
+                onClick={() => setShowInput(true)}
+              >
+                Edit
+              </button>
+            </div>
+          </>
         )}
-        <button
-          className="bg-blue-600 text-black font-bold rounded border border-white px-3 py-2 hover:bg-blue-500 hover:cursor-pointer"
-          onClick={() => setShowInput(!showInput)}
-        >
-          Toggle!
-        </button>
       </div>
     </>
   );
