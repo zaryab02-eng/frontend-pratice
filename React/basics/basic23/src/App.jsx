a38b7980e44d8d7f05ddc0f3e12e2da3c3d61437
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-09 09:21:32 +0530

    practicing

--- React/basics/basic23/src/App.jsx
+++ React/basics/basic23/src/App.jsx
@@ -2,23 +2,29 @@ import { useState } from "react";
 
 const App = () => {
   const [names] = useState(["Arshad", "Sabiha", "Zaryab", "Altamash"]);
+  const [clickedIndex, setClickedIndex] = useState(null);
+
   return (
-    <div className="bg-blue-100 p-6">
+    <>
       {names.map((name, index) => (
-        <p
-          className="text-black flex justify-between items-center h-12 w-32 "
+        <div
+          className="flex bg-amber-200 w-[12srem] h-[4rem] p-3 justify-between"
           key={index}
         >
-          {name}
+          {clickedIndex === index ? (
+            <input className="bg-white text-black px-2 py-1" type="text" />
+          ) : (
+            <p className="bg-white text-black px-2 py-1">{name}</p>
+          )}
           <button
-            className="bg-blue-600 cursor-pointer hover:bg-blue-500 px-3 py-2 rounded"
-            onClick={() => console.log(index)}
+            className="bg-red-600 px-2 py-1"
+            onClick={() => setClickedIndex(index)}
           >
             Edit
           </button>
-        </p>
+        </div>
       ))}
-    </div>
+    </>
   );
 };
 
