cc7c6050fb00c99c077871687558d84d8ec28ea9
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-09 17:54:43 +0530

    working on ui

--- React/Mini-Projects/ToDo App/src/App.jsx
+++ React/Mini-Projects/ToDo App/src/App.jsx
@@ -1,40 +1,21 @@
-import { useState } from "react";
+import { TiTick } from "react-icons/ti";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const App = () => {
-  const [temp, setTemp] = useState(20);
-
   return (
     <>
-      <div className="flex flex-col gap-y-2">
-        <p className="px-2.5 py-2 bg-white rounded text-black">
-          Temperature {temp}°
-        </p>
-        <div className="flex gap-x-0.5">
-          <button
-            className="px-1.5 py-1 bg-green-600 hover:cursor-pointer hover:bg-green-400"
-            onClick={() => setTemp(temp + 1)}
-          >
-            Increase
-          </button>
-          <button
-            className="px-1.5 py-1 bg-red-600 hover:cursor-pointer hover:bg-red-400"
-            onClick={() => setTemp(temp - 1)}
-          >
-            Decrease
-          </button>
+      <div className="bg-[#fefefe] h-[16rem] w-[19rem] shadow-[0_10px_30px_rgba(0,0,0,0.3)] border rounded-2xl">
+        <div className="flex justify-between p-3">
+          <div className="flex items-center space-x-2">
+            <div className="flex w-max h-max rounded-full p-[0.5px] border border-green-600">
+              <TiTick className="text-green-500 text-[13px]" />
+            </div>
+            <h1 className="text-black text-2xl font-bold">Todo</h1>
+          </div>
+          <div className="flex justify-center items-center">
+            <FaRegUserCircle className="text-black text-[18px]" />
+          </div>
         </div>
-        {temp < 0 && (
-          <p className="text-blue-500 font-semibold text-lg">Freezing!🥶</p>
-        )}
-        {temp >= 0 && temp < 15 && (
-          <p className="text-cyan-600 font-semibold text-lg">Cold!🧥</p>
-        )}
-        {temp >= 15 && temp < 25 && (
-          <p className="text-green-600 font-semibold text-lg">Nice!😊</p>
-        )}
-        {temp >= 25 && (
-          <p className="text-red-500 font-semibold text-lg">Hot!🔥</p>
-        )}
       </div>
     </>
   );
