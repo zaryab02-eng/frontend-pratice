22c32df0de2906b70987584a3eb23458b5dffcca
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-10 17:26:37 +0530

    practicing

--- React/basics/LocalStorage/basic01/src/App.jsx
+++ React/basics/LocalStorage/basic01/src/App.jsx
@@ -1,40 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  const [temp, setTemp] = useState(20);
+  const [myName, setmyName] = useState("");
+
+  const savedName = () => {
+    localStorage.setItem("My name", myName);
+    alert("Name Saved");
+  };
 
   return (
     <>
-      <div className="flex flex-col gap-y-2">
-        <p className="px-2.5 py-2 bg-white rounded text-black">
-          Temperature {temp}°
-        </p>
-        <div className="flex gap-x-0.5">
-          <button
-            className="px-1.5 py-1 bg-green-600 hover:cursor-pointer hover:bg-green-400"
-            onClick={() => setTemp(temp + 1)}
-          >
-            Increase
-          </button>
-          <button
-            className="px-1.5 py-1 bg-red-600 hover:cursor-pointer hover:bg-red-400"
-            onClick={() => setTemp(temp - 1)}
-          >
-            Decrease
-          </button>
-        </div>
-        {temp < 0 && (
-          <p className="text-blue-500 font-semibold text-lg">Freezing!🥶</p>
-        )}
-        {temp >= 0 && temp < 15 && (
-          <p className="text-cyan-600 font-semibold text-lg">Cold!🧥</p>
-        )}
-        {temp >= 15 && temp < 25 && (
-          <p className="text-green-600 font-semibold text-lg">Nice!😊</p>
-        )}
-        {temp >= 25 && (
-          <p className="text-red-500 font-semibold text-lg">Hot!🔥</p>
-        )}
+      <div className="space-x-1">
+        <input
+          value={myName}
+          onChange={(e) => setmyName(e.target.value)}
+          type="text"
+          className="bg-white text-black px-2 py-1"
+          placeholder="Please enter your name"
+        />
+        <button
+          onClick={savedName}
+          className="bg-blue-600 text-white font-bold rounded px-2 py-1"
+        >
+          Save
+        </button>
       </div>
     </>
   );
