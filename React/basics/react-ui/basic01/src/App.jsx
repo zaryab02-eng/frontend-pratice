bd7c9cfd6a958e241211110df9e6fecada4350b9
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-14 20:09:17 +0530

    practicing

---
+++ React/basics/react-ui/basic01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "./components/Greeting";
+
+const App = () => {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+};
+
+export default App;
---
+++ React/basics/react-ui/basic01/src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+const Greeting = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-600 text-white">Hello, React!</h1>
+      <p className="text-white">
+        This is my first component, Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Similique omnis voluptatum quam inventore nisi alias
+        expedita, quod architecto laborum soluta. Libero, est unde. Fuga dolore
+        esse quisquam quas doloribus consequuntur. Quam, voluptates modi dolorem
+        odit, provident quod quo nihil cumque quisquam vero reprehenderit nobis,
+        quasi ratione eius sunt delectus enim tempore. Sunt, odit! Quae nulla,
+        quo praesentium repudiandae hic sapiente.
+      </p>
+    </div>
+  );
+};
+
+export default Greeting;
