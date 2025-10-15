591d0fa4160b24a8aa08cf9eb695fb30266d0e45
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-15 19:40:50 +0530

    practcing

--- React/basics/react-ui/basic03/src/assets/components/ProductCard.jsx
+++ React/basics/react-ui/basic03/src/assets/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 const ProductCard = ({ product }) => {
   return (
     <div className="p-6">
-      <div className="h-[21rem] w-64 bg-white rounded-2xl shadow-2xl flex flex-col justify-between p-5 py-[20px] ">
+      <div className="h-[20rem] w-64 bg-white rounded-2xl shadow-2xl flex flex-col justify-between p-5 py-[20px] ">
         <div className="relative">
           <img
             src={product.image}
@@ -11,26 +11,25 @@ const ProductCard = ({ product }) => {
           <div className="bg-[#E53431] absolute top-0 right-0 p-2 h-min w-min rounded-tr-2xl text-[15px] rounded-bl-[4rem] flex justify-end items-start">
             <span>{product.discount}%</span>
           </div>
-        </div>
-        <div className="text-black">
-          <div className="flex justify-end relative top-[-1rem]">
-            <span className="text-[12px] font-semibold">
+          <div className="flex justify-end mt-2">
+            <span className="text-[12px] font-semibold text-black">
               {product.rating}⭐
             </span>
           </div>
+        </div>
+        <div className="text-black">
           <div className="flex text-[18px] justify-center">
             <h1 className="font-bold">{product.title}</h1>
           </div>
           <div className="flex justify-start items-center gap-1.5 relative left-2 top-2">
             <span className="text-[13px] line-through text-[#ACACAC] decoration-[#ACACAC]">
-              {product.oldPrice}
+              ${product.oldPrice}
             </span>
             <span className="text-[#0D452C] font-bold text-[20px]">
-              {product.price}
+              ${product.price}
             </span>
           </div>
         </div>
-        <div className="absolute"></div>
         <button className="text-white bg-gradient-to-r from-blue-400 to-blue-500 font-bold py-1 rounded-full relative bottom-0 cursor-pointer">
           ADD TO CART
         </button>
