898d7b22030863235081419ba83e5267e75d8b31
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-18 14:58:08 +0530

    practicing

--- React/basics/react-ui/basic03/src/assets/components/Hero.jsx
+++ React/basics/react-ui/basic03/src/assets/components/Hero.jsx
@@ -3,7 +3,7 @@ import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 const Hero = () => {
   return (
-    <div className="h-dvh">
+    <div className="h-screen bg-amber-600">
       <img src={Herobg} alt="Hero" className="h-screen" />
       <button className="bg-gradient-to-r from-blue-400 to-blue-600 px-14 py-4.5 text-4xl rounded-full font-semibold absolute top-[82.15%] left-[5.85%] ">
         SHOP NOW
