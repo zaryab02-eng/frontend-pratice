29688db80302c6b1602c9195b0c3085148ad61ed
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-20 16:53:07 +0530

    Practicing

--- React/basics/react-ui/basic03/src/assets/components/HeroComp/HeroPara.jsx
+++ React/basics/react-ui/basic03/src/assets/components/HeroComp/HeroPara.jsx
@@ -1,7 +1,7 @@
 const HeroPara = () => {
   return (
-    <div className="w-80 md:w-[30rem] pl-6 pt-24">
-      <p className="text-white text-[0.9rem] md:text-[1.4em]">
+    <div className="w-80 md:w-[30rem] sm:w-xl pl-6 pt-24">
+      <p className="text-white text-[0.9rem] sm:text-2xl md:text-[1.4em]">
         This is a space to welcome visitors to the site. <br /> Grab their
         attention with copy that clearly states what the site is about.
       </p>
--- React/basics/react-ui/basic03/src/assets/components/Navbar/Nav.jsx
+++ React/basics/react-ui/basic03/src/assets/components/Navbar/Nav.jsx
@@ -3,9 +3,11 @@ import { IoMenuSharp } from "react-icons/io5";
 const Nav = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-14 md:h-20 bg-transparent flex items-center justify-between px-6 md:px-12 z-50 text-white md:bg-gray-800/20 md:backdrop-blur-sm">
-      <h2 className="font-bold cursor-pointer md:text-3xl">LUMINU</h2>
+      <h2 className="font-bold cursor-pointer sm:text-3xl md:text-3xl">
+        LUMINU
+      </h2>
       <div className="hidden">Menu</div>
-      <span className="text-3xl cursor-pointer md:text-4xl">
+      <span className="text-3xl cursor-pointer sm:text-4xl md:text-4xl">
         <IoMenuSharp />
       </span>
     </div>
