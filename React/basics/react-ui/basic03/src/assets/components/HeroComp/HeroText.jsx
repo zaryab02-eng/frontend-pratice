0aeacf6d74837a48e0ef556b38200d554fdcb82e
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-21 16:20:54 +0530

    ui taking so long

--- React/basics/react-ui/basic03/src/assets/components/HeroComp/HeroText.jsx
+++ React/basics/react-ui/basic03/src/assets/components/HeroComp/HeroText.jsx
@@ -1,7 +1,7 @@
 const HeroText = () => {
   return (
     <div>
-      <h1 className="text-white font-bold z-50 pt-44 sm:pt-72 sm:pl-9 pl-6 lg:pl-12 text-[1.900rem] leading-9 sm:text-5xl md:text-6xl sm:leading-[3.8rem] md:leading-18">
+      <h1 className="text-white font-bold z-50 pt-44 sm:pt-72 sm:pl-9 pl-6 md:pt-52 lg:pl-12 text-[1.900rem] leading-9 sm:text-5xl md:text-[3.200rem] sm:leading-[3.8rem] md:leading-16">
         FUTURE-READY, <br /> SMART BANKING{" "}
       </h1>
     </div>
--- React/basics/react-ui/basic03/src/assets/components/Navbar/Nav.jsx
+++ React/basics/react-ui/basic03/src/assets/components/Navbar/Nav.jsx
@@ -2,8 +2,8 @@ import { IoMenuSharp } from "react-icons/io5";
 
 const Nav = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-14 sm:h-[3.125rem] md:h-20 lg:h-[4.9rem] bg-transparent flex items-center justify-between px-6 sm:px-7 sm:py-7 md:px-12 z-50 text-white md:bg-gray-800/20 md:backdrop-blur-sm">
-      <h2 className="font-bold cursor-pointer sm:text-[1.125rem] md:text-3xl lg:text-[1.7rem]">
+    <div className="fixed top-0 left-0 w-full h-14 sm:h-[3.125rem] md:h-[4.0625rem] lg:h-[4.9rem] bg-transparent flex items-center justify-between px-6 sm:px-7 sm:py-7 md:px-10 md:py-2 z-50 text-white md:bg-gray-800/20 md:backdrop-blur-sm">
+      <h2 className="font-bold cursor-pointer sm:text-[1.125rem] md:text-2xl lg:text-[1.7rem]">
         LUMINU
       </h2>
       <ul className="hidden lg:flex lg:gap-x-9 lg:text-[1.2rem] lg:relative lg:right-[17rem]">
