fafa6a2966bb8b093dd63fcf563c9e0d5b939b52
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-23 20:32:53 +0530

    basic ui done for card now ill make it responsive also

--- React/basics/react-ui/basic03/src/assets/components/Section2/SectionTexts.jsx
+++ React/basics/react-ui/basic03/src/assets/components/Section2/SectionTexts.jsx
@@ -1,6 +1,6 @@
 const SectionTexts = () => {
   return (
-    <div className="text-black pt-16 w-72">
+    <div className="text-black pt-16 w-72 mb-12">
       <h4 className="text-[0.8413rem] font-semibold">FINANCIAL TOOLS</h4>
       <h2 className="text-[1.3221rem]">Streamline Your Banking</h2>
       <p className="text-[0.78125rem] pt-3.5">
