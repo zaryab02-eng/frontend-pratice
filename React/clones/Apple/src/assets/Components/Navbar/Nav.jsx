7d3e50a31a4cb5c4545266235c6c9179d3bdaea2
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-28 21:58:39 +0530

    nav done

--- React/clones/Apple/src/assets/Components/Navbar/Nav.jsx
+++ React/clones/Apple/src/assets/Components/Navbar/Nav.jsx
@@ -3,7 +3,7 @@ import Nav2 from "./Nav2";
 import Nav3 from "./Nav3";
 const Nav = () => {
   return (
-    <div className="h-12 bg-amber-300 w-full flex justify-between pt-3 px-3  text-[1.3rem]  xl:flex xl:justify-center xl:gap-x-[2.5rem]">
+    <div className="h-12 bg-amber-300 w-full flex justify-between lg:flex lg:justify-center lg:gap-x-[2.5rem] pt-3 px-3  text-[1.3rem]  xl:flex xl:justify-center xl:gap-x-[2.5rem]">
       <Nav1 />
       <Nav3 />
       <Nav2 />
--- React/clones/Apple/src/assets/Components/Navbar/Nav2.jsx
+++ React/clones/Apple/src/assets/Components/Navbar/Nav2.jsx
@@ -5,7 +5,7 @@ import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 const Nav2 = () => {
   return (
-    <div className="flex gap-x-7 xl:text-[1.2rem] xl:gap-[2.3rem] xl:relative xl:pr-3.5">
+    <div className="flex gap-x-7 xl:text-[1.2rem] xl:gap-[2.3rem] xl:relative xl:pr-3.5 lg:relative lg:pr-2.5">
       <IoIosSearch />
       <BiShoppingBag />
       <HiOutlineMenuAlt4 className="lg:hidden xl:hidden" />
