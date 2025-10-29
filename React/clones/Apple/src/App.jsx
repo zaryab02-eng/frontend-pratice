48d8828cd71eed9a1c6fc32193dbeed37e61649c
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-29 15:50:11 +0530

    working

--- React/clones/Apple/src/App.jsx
+++ React/clones/Apple/src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./assets/Components/Navbar/Nav";
+import NavHeading from "./assets/Components/Navbar2/NavHeading";
 
 const App = () => {
   return (
     <>
       <Nav />
+      <NavHeading />
     </>
   );
 };
---
+++ React/clones/Apple/src/assets/Components/Navbar2/NavHeading.jsx
@@ -0,0 +1,18 @@
+import { CiCirclePlus } from "react-icons/ci";
+const NavHeading = () => {
+  return (
+    <div className="h-[6.125rem] flex justify-center items-center px-6 sm:px-10 ">
+      <div className="w-max leading-5 text-[0.850rem] flex text-center">
+        <p>
+          Get up to 6 months of No Cost EMI plus up to ₹10000.00 instant
+          cashback on selected products with eligible cards.
+          <span className="text-blue-700 font-semibold inline-flex items-center gap-1 relative left-0.5">
+            See offers <CiCirclePlus />
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NavHeading;
