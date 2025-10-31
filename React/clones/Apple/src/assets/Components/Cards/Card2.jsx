23ab5bfbafc03b1e2413e1e071a3e8cf45b28e5f
zaryab02-eng <mohdzaryab99@gmail.com>
2025-10-31 16:24:45 +0530

    working

--- React/clones/Apple/src/assets/Components/Cards/Card2.jsx
+++ React/clones/Apple/src/assets/Components/Cards/Card2.jsx
@@ -6,7 +6,7 @@ const Card2 = () => {
       {Card2Data.map((product) => (
         <div
           key={product.id}
-          className="h-[28.125rem] w-[19.3125rem] md:w-[19rem] md:h-[23rem] shrink-0 rounded-2xl relative mt-6 pl-7 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center"
+          className="h-[28.125rem] w-[19.3125rem] md:w-[19rem] lg:w-[25rem] lg:h-[31.25rem] md:h-[23rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center"
           style={{ backgroundImage: `url('${product.image}')` }}
         >
           <div
@@ -16,15 +16,15 @@ const Card2 = () => {
           >
             <a
               href="#"
-              className="text-[1.3125rem] font-semibold tracking-tight md:text-[1.4rem] md:tracking-tighter"
+              className="text-[1.3125rem] font-semibold lg:tracking-wider tracking-tight md:text-[1.4rem] lg:text-[1.8rem] md:tracking-tighter font-[SFProDisplay]"
             >
               {product.name}
             </a>
-            <div className="flex flex-col gap-y-0.5">
-              <span className="text-[1.0625rem] md:text-[0.9em] tracking-normal">
+            <div className="flex flex-col gap-y-0.5 font-[SFProDisplay]">
+              <span className="text-[1.0625rem] md:text-[0.9em] lg:text-[1.1rem] tracking-normal lg:tracking-wider">
                 {product.highlight}
               </span>
-              <span className="text-[0.875rem] md:text-[0.7em]  tracking-wide">
+              <span className="text-[0.875rem] md:text-[0.7em] lg:text-[0.8rem] tracking-wide font-[SFProDisplay] lg:tracking-wider">
                 {product.price}
               </span>
             </div>
--- React/clones/Apple/src/assets/Components/Sections/Section1/Latest.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/Latest.jsx
@@ -2,8 +2,8 @@ import Card2 from "../../Cards/Card2";
 
 const Latest = () => {
   return (
-    <div className="w-full h-[34.625rem] font-semibold tracking-wider pl-6 md:pl-5 bg-amber-400 md:relative md:bottom-42 lg:relative lg:mt-40">
-      <h2 className="text-2xl md:text-[1.4rem] md:mt-1 font-[SFProDisplay] leading-7">
+    <div className="w-full h-[34.625rem] lg:h-[36rem] font-semibold tracking-wider pl-6 md:pl-5 bg-amber-400 md:relative md:bottom-42 lg:relative lg:bottom-44 lg:mt-40">
+      <h2 className="text-2xl md:text-[1.4rem] lg:text-[1.8rem] md:mt-1 lg:relative lg:pb-3 font-[SFProDisplay] lg:tracking-wide leading-7">
         The latest.&nbsp;
         <span className="text-[#76767B]">
           Take a look at <br className="sm:hidden" /> what's new right now.
