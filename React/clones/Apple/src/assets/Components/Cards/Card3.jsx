511076ac5e05da4befc6aa3a2349be6018763f8f
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-03 19:13:05 +0530

    working

--- React/clones/Apple/src/assets/Components/Cards/Card3.jsx
+++ React/clones/Apple/src/assets/Components/Cards/Card3.jsx
@@ -3,13 +3,13 @@ const Card2 = () => {
   return (
     <div
       id="card1"
-      className="flex overflow-x-auto -ml-6 gap-x-5 sm:gap-x-5 relative pr-6 xl:relative xl:pl-18 xl:pr-24 xl:bottom-1 snap-x snap-mandatory snap-smooth"
+      className="flex overflow-x-auto -ml-6 gap-x-5 sm:gap-x-5 relative pr-6 xl:relative xl:pl-18 xl:pr-24 xl:bottom-1 snap-x snap-mandatory snap-smooth sm:snap-x sm:snap-mandatory sm:snap-smooth"
     >
       {Card3Data.map((product) => (
         <div
           key={product.id}
-          className={`h-[28.125rem] w-[19.3125rem] snap-center md:w-[19rem] lg:w-[25rem] lg:h-[31.25rem] md:h-[23rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center ${
-            product.id === 1 ? "relative pt-17 ml-12" : ""
+          className={`h-[28.125rem] w-[19.3125rem] snap-center sm:snap-end md:w-[19rem] lg:w-[25rem] lg:h-[31.25rem] md:h-[23rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center ${
+            product.id === 1 ? "relative pt-17 ml-12 sm:relative sm:pt-10" : ""
           } ${product.id === 7 ? "relative" : ""}`}
           style={{ backgroundImage: `url('${product.image}')` }}
         >
--- React/clones/Apple/src/assets/Components/Sections/Section1/HelpIsHere.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/HelpIsHere.jsx
@@ -2,7 +2,7 @@ import Card3 from "../../Cards/Card3";
 
 const Latest = () => {
   return (
-    <div className="w-full h-[34.625rem] lg:h-[36rem] font-semibold tracking-wider md:pl-5 md:relative md:bottom-42 lg:relative lg:bottom-44 lg:mt-40">
+    <div className="w-full h-[34.625rem] lg:h-[36rem] sm:-mt-8 font-semibold tracking-wider md:pl-5 md:relative md:bottom-42 lg:relative lg:bottom-44 lg:mt-40">
       <h2 className="text-2xl md:text-[1.4rem] ml-6 lg:text-[1.8rem] lg:relative lg:pb-3 font-[SFProDisplay] xl:tracking-wide leading-7">
         Help is here.&nbsp;
         <span className="text-[#76767B]">
