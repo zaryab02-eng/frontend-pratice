65302c0fc3e9306290005062372369d97fe89971
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-05 14:07:11 +0530

    card 3 completed

--- React/clones/Apple/src/assets/Components/Cards/Card3.jsx
+++ React/clones/Apple/src/assets/Components/Cards/Card3.jsx
@@ -3,27 +3,34 @@ const Card2 = () => {
   return (
     <div
       id="card1"
-      className="flex overflow-x-auto -ml-6 gap-x-5 md:gap-x-4 sm:gap-x-5 relative pr-6 xl:relative xl:pl-18 xl:pr-24 xl:bottom-1 snap-x snap-mandatory snap-smooth sm:snap-x sm:snap-mandatory sm:snap-smooth"
+      className="flex overflow-x-auto -ml-6 gap-x-5 md:gap-x-4 lg:gap-x-5 sm:gap-x-5 relative pr-6 xl:relative xl:pl-18 xl:pr-24 xl:bottom-25 "
     >
       {Card3Data.map((product) => (
         <div
           key={product.id}
-          className={`h-[28.125rem] w-[19.3125rem] md:font-light snap-center sm:snap-end lg:w-[25rem] lg:h-[31.25rem] md:h-[24rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center ${
+          className={`h-[28.125rem] w-[19.3125rem] md:font-light lg:pr-2 lg:h-[31.25rem] md:h-[24rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center ${
             product.id === 1 ? "relative pt-17 ml-12 sm:relative sm:pt-10" : ""
-          }   ${product.id === 5 ? "md:w-[18rem]" : "md:w-[22rem]"} `}
+          }   ${
+            product.id === 5
+              ? "md:w-[18rem] lg:w-[25rem] xl:w-[25rem]"
+              : "md:w-[22rem] lg:w-[31rem] xl:w-[30rem]"
+          } `}
           style={{ backgroundImage: `url('${product.image}')` }}
         >
           <div
             className={`${
               product.theme === "light" ? "text-black" : "text-white"
-            }  flex flex-col relative bottom-6 font-medium`}
+            }  flex flex-col relative bottom-6 font-medium `}
           >
-            <a href="#" className="text-[0.7rem] ">
+            <a
+              href="#"
+              className="text-[0.7rem] md:text-[0.6rem] lg:text-[0.7rem] md:relative md:top-1.5"
+            >
               {product.tag}
             </a>
             <a
               href="#"
-              className={`text-[1.3000rem] leading-6 relative top-2.5 tracking-wider font-semibold lg:tracking-wider md:text-[1.4rem] lg:text-[1.8rem] md:tracking-normal font-[SFProDisplay] ${
+              className={`text-[1.3000rem] leading-6 relative top-2.5 tracking-wider font-semibold md:text-[1.4rem] lg:text-[1.77rem] lg:tracking-wide lg:leading-8 md:tracking-normal font-[SFProDisplay] ${
                 product.id === 1 ? "md:relative md:mt-4 md:ml-1 " : ""
               }`}
             >
@@ -31,7 +38,7 @@ const Card2 = () => {
             </a>
             <div className="flex flex-col gap-y-0.5 font-[SFProDisplay]">
               <span
-                className={`text-[1.0525rem] md:text-[0.9rem] md:tracking-tight md:leading-4 leading-5 relative top-4 font-medium text-gray-900 lg:text-[0.8rem] tracking-wide font-[SFProDisplay] lg:tracking-wider ${
+                className={`text-[1.0525rem] md:text-[0.9rem] lg:text-[1.2rem] md:tracking-tight md:leading-4 leading-5 relative top-4 font-medium text-gray-900 lg:leading-6 tracking-wide font-[SFProDisplay] lg:tracking-normal ${
                   product.id === 3
                     ? "leading-5 tracking-wide text-[1.0625rem]"
                     : ""
