ace9326ac561b25807bff2f6fa4b3678ff1ef363
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-06 11:11:54 +0530

    working

--- React/clones/Apple/src/assets/Components/Cards/Card4.jsx
+++ React/clones/Apple/src/assets/Components/Cards/Card4.jsx
@@ -1,32 +1,5 @@
-import { Card2Data } from "../../../data/Card2Data.js";
-
 const Card4 = () => {
-  return (
-    <div
-      id="card1"
-      className="flex overflow-x-auto gap-x-4 sm:gap-x-5 relative pr-6 md:-mt-1 xl:relative xl:pl-18 xl:pr-24 xl:bottom-1"
-    >
-      {Card2Data.map((product) => (
-        <div
-          key={product.id}
-          className="h-[15rem] bg-amber-600 w-[19.3125rem] md:w-[19rem] lg:w-[25rem] lg:h-[31.25rem] md:h-[23rem] shrink-0 rounded-2xl relative mt-6 pl-7 lg:pl-8 lg:pt-11 md:pl-5 md:pt-9 pt-12 flex flex-col justify-start bg-cover bg-center"
-        >
-          <div
-            className={`${
-              product.theme === "light" ? "text-black" : "text-white"
-            }`}
-          >
-            <a
-              href="#"
-              className="text-[1.3125rem] font-semibold lg:tracking-wider tracking-tight md:text-[1.4rem] lg:text-[1.8rem] md:tracking-tighter font-[SFProDisplay]"
-            >
-              No Cost EMI. Plus Instant Cashback.
-            </a>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>Card4</div>;
 };
 
 export default Card4;
--- React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
@@ -2,7 +2,7 @@ import Card4 from "../../Cards/Card4";
 
 const Apple = () => {
   return (
-    <div className="w-full h-[34.625rem] lg:h-[36rem] sm:-mt-8 font-bold tracking-wider md:ml-[-0.7rem] md:pr-2 md:relative md:bottom-63 lg:relative lg:bottom-44 lg:mt-40">
+    <div className="w-full h-[34.625rem] lg:h-[36rem] sm:-mt-8 top-5 relative font-semibold tracking-wider md:ml-[-0.7rem] md:pr-2 md:relative md:bottom-63 lg:relative lg:bottom-44 lg:mt-40">
       <h2 className="text-2xl md:text-[1.4rem] md:font-normal ml-6 lg:text-[1.9rem] xl:bottom-28  lg:tracking-tight lg:left-2.5 lg:bottom-0.5 lg:relative font-[SFProDisplay]  xl:font-semibold xl:text-[1.8rem] md:tracking-normal leading-7 xl:relative xl:left-20 xl:tracking-wide">
         The Apple Store difference. <br />
         <span className="text-[#76767B]">
