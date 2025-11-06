dfea2ace74c07c702fa424c1503734aa2596c76d
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-07 01:32:45 +0530

    working

--- React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
@@ -1,15 +1,16 @@
 const Apple = () => {
   return (
-    <div className="w-full h-[34.625rem] lg:h-[36rem] sm:-mt-8 top-6 relative font-semibold tracking-wider md:ml-[-0.7rem] md:pr-2 md:relative md:bottom-63 lg:relative lg:bottom-44 lg:mt-40">
-      <h2 className="text-2xl md:text-[1.4rem] md:font-normal ml-6 lg:text-[1.9rem] xl:bottom-28  lg:tracking-tight lg:left-2.5 lg:bottom-0.5 lg:relative font-[SFProDisplay]  xl:font-semibold xl:text-[1.8rem] md:tracking-normal leading-7 xl:relative xl:left-20 xl:tracking-wide">
-        The Apple Store difference. <br className="sm:hidden" />
+    <section className="w-full py-10 font-semibold tracking-wider overflow-hidden">
+      <h2 className="text-2xl md:text-[1.4rem] md:font-normal px-6 lg:text-[1.9rem] xl:text-[1.8rem] font-[SFProDisplay] text-balance leading-snug">
+        The Apple Store difference.
+        <br className="sm:hidden" />
         <span className="text-[#76767B]">
-          Even more reasons to <br className="hidden sm:flex" /> shop
+          Even more reasons to
+          <br className="hidden sm:flex" /> shop
           <br className="sm:hidden" /> with us.
         </span>
       </h2>
-      <div className="relative top-2 sm:relative sm:top-2.5"></div>
-    </div>
+    </section>
   );
 };
 
