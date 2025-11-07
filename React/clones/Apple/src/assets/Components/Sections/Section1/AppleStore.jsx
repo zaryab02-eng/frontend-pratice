7cbebd022d4f6d1e72d02f99c15246f618490358
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-07 16:48:11 +0530

    working on card 4

--- React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/AppleStore.jsx
@@ -1,7 +1,9 @@
+import Card4 from "../../Cards/Card4";
+
 const Apple = () => {
   return (
-    <section className="w-full py-10 font-semibold tracking-wider overflow-hidden">
-      <h2 className="text-2xl md:text-[1.4rem] md:font-normal px-6 lg:text-[1.9rem] xl:text-[1.8rem] font-[SFProDisplay] text-balance leading-snug">
+    <section className="w-full py-10 font-semibold tracking-wider overflow-hidden relative bottom-4">
+      <h2 className="text-2xl md:text-[1.4rem] md:font-normal px-6 lg:text-[1.9rem] xl:text-[1.8rem] font-[SFProDisplay] text-balance leading-[1.80rem]">
         The Apple Store difference.
         <br className="sm:hidden" />
         <span className="text-[#76767B]">
@@ -10,6 +12,9 @@ const Apple = () => {
           <br className="sm:hidden" /> with us.
         </span>
       </h2>
+      <section>
+        <Card4 />
+      </section>
     </section>
   );
 };
