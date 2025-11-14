11bb8d4510468e39bc56ab860c74b4cc299da37f
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-14 11:48:00 +0530

    working on detail

--- React/clones/Apple/src/assets/Components/Navbar/Nav.jsx
+++ React/clones/Apple/src/assets/Components/Navbar/Nav.jsx
@@ -3,7 +3,7 @@ import Nav2 from "./Nav2";
 import Nav3 from "./Nav3";
 const Nav = () => {
   return (
-    <div className="h-12 w-full flex justify-between lg:flex lg:justify-center lg:gap-x-[2.5rem] pt-3 px-3  text-[1.3rem]  xl:flex xl:justify-center xl:gap-x-[2.5rem]">
+    <div className="h-12 z-50 w-full flex justify-between lg:flex lg:justify-center lg:gap-x-[2.5rem] pt-3 px-3  text-[1.3rem] xl:flex xl:justify-center xl:gap-x-[2.5rem] relative">
       <Nav1 />
       <Nav3 />
       <Nav2 />
--- React/clones/Apple/src/assets/Components/Sections/Section1/NavHeading.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/NavHeading.jsx
@@ -4,10 +4,13 @@ import { CiCirclePlus } from "react-icons/ci";
 const NavHeading = () => {
   return (
     <Motion.div
-      initial={{ opacity: 0, y: -25 }}
+      initial={{ opacity: 0, y: -60 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
-      className="h-[6.125rem] flex justify-center items-center px-8 sm:px-10 sm:pb-3.5 md:relative md:bottom-7 lg:relative lg:pt-3.5 xl:relative xl:pt-6 xl:right-6"
+      transition={{
+        duration: 1.5,
+        ease: [0.22, 1, 0.36, 1],
+      }}
+      className="h-min w-full bg-[#FFFFFF] z-0 flex justify-center items-center py-5 relative bottom-4.5 mt-4.5 px-8 sm:px-10 sm:pb-3.5 md:relative md:bottom-7 lg:relative lg:pt-3.5 xl:relative xl:pt-6 xl:right-6"
     >
       <div className="w-max leading-5 text-[0.9rem] md:text-[0.7rem] lg:text-[0.9rem] flex text-center">
         <p className="font-[SFProDisplay] tracking-wide">
