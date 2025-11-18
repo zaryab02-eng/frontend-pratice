4bc779400a396840a799a95738e11b32ee0fab5e
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-18 21:45:24 +0530

    working

--- React/clones/Apple/src/assets/Components/Cards/Card5.jsx
+++ React/clones/Apple/src/assets/Components/Cards/Card5.jsx
@@ -20,7 +20,7 @@ const Card5 = () => {
           <div
             className={`${
               product.theme === "light" ? "text-black" : "text-white"
-            }  flex flex-col relative bottom-6 font-medium `}
+            }  flex flex-col relative bottom-10 font-medium `}
           >
             <a
               href="#"
