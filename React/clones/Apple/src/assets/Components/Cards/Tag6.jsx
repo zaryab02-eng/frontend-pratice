654edd9604df04299329e03e0c4dd9964e988f47
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-22 15:45:42 +0530

    working

---
+++ React/clones/Apple/src/assets/Components/Cards/Tag6.jsx
@@ -0,0 +1,25 @@
+const Tag6 = () => {
+  return (
+    <>
+      <div className="flex relative left-6 gap-x-1.5 font-[SFProDisplay] font-normal">
+        <div className="border border-black rounded-full px-3 py-1.5 text-[0.875rem] whitespace-nowrap">
+          <a href="#">Find a Store</a>
+        </div>
+
+        <div className="border border-black rounded-full px-3 py-1.5 text-[0.875rem] whitespace-nowrap ">
+          <a href="#">Order Status</a>
+        </div>
+
+        <div className="border border-black rounded-full px-3 py-1.5 text-[0.875rem] whitespace-nowrap ">
+          <a href="#">Shopping Help</a>
+        </div>
+
+        <div className="border border-black rounded-full px-3 py-1.5 text-[0.875rem] whitespace-nowrap ">
+          <a href="#">Your Saves</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tag6;
