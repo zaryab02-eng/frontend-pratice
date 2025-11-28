c5ec4ef655a11e0ed836485b55f001b0c4cce9a6
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-28 16:03:16 +0530

    for lg screen done till quick link

--- React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
@@ -1,27 +1,27 @@
 const Quick = () => {
   return (
-    <div className=" md:-translate-x-1">
+    <div className=" md:-translate-x-1 lg:-translate-y-23 lg:-translate-x-0">
       {/* Only translate the heading container */}
       <div className="sm:translate-y-5">
-        <h2 className="text-black text-[1.3rem] font-[SFProDisplay] font-semibold tracking-wider sm:tracking-wide -translate-y-4.5 translate-x-5.5">
+        <h2 className="text-black text-[1.3rem] lg:text-[1.8rem] font-[SFProDisplay] font-semibold tracking-wider sm:tracking-wide -translate-y-4.5 translate-x-5.5">
           Quick Links
         </h2>
       </div>
-      <div className="mt-1 sm:mt-1.5 md:mt-[0rem] md:ml-[-0.2rem]">
+      <div className="mt-1 sm:mt-1.5 md:mt-[0rem] md:ml-[-0.2rem] lg:mt-2">
         <div
           id="card1"
           className="w-full py-3 pl-6 pr-5.5 flex items-center gap-2 overflow-x-auto whitespace-nowrap will-change-transform sm:tracking-wide"
         >
-          <span className="text-[0.9rem] md:text-[0.7rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 border-1 border-black rounded-full">
+          <span className="text-[0.9rem] lg:text-[0.95rem] md:text-[0.7rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 lg:px-3.5 border-1 border-black rounded-full">
             Find a Store
           </span>
-          <span className="text-[0.9rem] md:text-[0.7rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 border-1 border-black rounded-full">
+          <span className="text-[0.9rem] md:text-[0.7rem] lg:text-[0.95rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 lg:px-3.5 border-1 border-black rounded-full">
             Order Status
           </span>
-          <span className="text-[0.9rem] md:text-[0.7rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 border-1 border-black rounded-full">
+          <span className="text-[0.9rem] md:text-[0.7rem] lg:text-[0.95rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 lg:px-3.5 border-1 border-black rounded-full">
             Shopping Help
           </span>
-          <span className="text-[0.9rem] md:text-[0.7rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 border-1 border-black rounded-full">
+          <span className="text-[0.9rem] md:text-[0.7rem] lg:text-[0.95rem] font-[SFProDisplay] px-4 py-2 md:py-1.5 md:px-3 lg:px-3.5 border-1 border-black rounded-full">
             Your Saves
           </span>
         </div>
