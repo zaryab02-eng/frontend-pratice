543b796ae19ad459a7db58744d6864c35b408395
zaryab02-eng <mohdzaryab99@gmail.com>
2025-11-28 13:10:50 +0530

    working

--- React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
@@ -1,24 +1,28 @@
 const Quick = () => {
   return (
-    <div className="sm:translate-y-5">
-      <h2 className="text-black text-[1.3rem] font-[SFProDisplay] font-semibold tracking-wider sm:tracking-wide -translate-y-4.5 translate-x-5.5">
-        Quick Links
-      </h2>
-      <div className="-translate-y-3.5 -translate-x-0.5">
+    <div>
+      {/* Only translate the heading container */}
+      <div className="sm:translate-y-5">
+        <h2 className="text-black text-[1.3rem] font-[SFProDisplay] font-semibold tracking-wider sm:tracking-wide -translate-y-4.5 translate-x-5.5">
+          Quick Links
+        </h2>
+      </div>
+
+      <div className="mt-1 sm:mt-1.5">
         <div
           id="card1"
-          className="w-full h-min py-3 pl-6 pr-5.5 flex items-center gap-2 overflow-x-auto"
+          className="w-full py-3 pl-6 pr-5.5 flex items-center gap-2 overflow-x-auto whitespace-nowrap will-change-transform sm:tracking-wide"
         >
-          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black w-max h-max rounded-full tracking-wide whitespace-nowrap">
+          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black rounded-full">
             Find a Store
           </span>
-          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black w-max h-max rounded-full tracking-wide whitespace-nowrap">
+          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black rounded-full">
             Order Status
           </span>
-          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black w-max h-max rounded-full tracking-wide whitespace-nowrap">
+          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black rounded-full">
             Shopping Help
           </span>
-          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black w-max h-max rounded-full tracking-wide whitespace-nowrap">
+          <span className="text-[0.9rem] font-[SFProDisplay] px-4 py-2 border-1 border-black rounded-full">
             Your Saves
           </span>
         </div>
