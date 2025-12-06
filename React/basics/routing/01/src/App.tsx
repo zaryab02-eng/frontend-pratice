2f95beb7c81346aa0655fa79d5521e52bbf333f6
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-06 23:30:40 +0530

    project base ready

--- React/basics/routing/01/src/App.tsx
+++ React/basics/routing/01/src/App.tsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
