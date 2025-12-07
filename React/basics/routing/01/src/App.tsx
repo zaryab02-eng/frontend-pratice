d2158492b1438f5dac9b73c681066216653d6d13
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-07 15:21:46 +0530

    understanding routig

--- React/basics/routing/01/src/App.tsx
+++ React/basics/routing/01/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./assets/Components/Home";
 import { About } from "./assets/Components/About";
 import Contact from "./assets/Components/Contact";
 import { Prouduct } from "./assets/Components/Prouduct";
+import NotFound from "./assets/Components/NotFound";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product" element={<Prouduct />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ React/basics/routing/01/src/assets/Components/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h5 className="text-red-600">404|Page Not Found</h5>
+    </div>
+  );
+};
+
+export default NotFound;
