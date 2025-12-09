74e4b031c49fec3240f9fd06adaac2351acce3d1
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-09 16:22:02 +0530

    working

--- React/basics/routing/01/src/assets/Components/Footer.tsx
+++ React/basics/routing/01/src/assets/Components/Footer.tsx
@@ -1,20 +1,26 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-gray-900 py-1.5 flex justify-center gap-x-2 w-full fixed bottom-0">
-      <Link
-        to={"-1"}
-        className="bg-red-600 px-3 py-1 rounded-2xl active:scale-95"
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="bg-red-600 px-3 py-1 rounded-2xl active:scale-95 cursor-pointer"
       >
         Previous
-      </Link>
-      <Link
-        to={"+1"}
-        className="bg-red-600 px-3 py-1 rounded-2xl active:scale-95"
+      </button>
+      <button
+        onClick={() => {
+          navigate(+1);
+        }}
+        className="bg-red-600 px-3 py-1 rounded-2xl active:scale-95 cursor-pointer"
       >
         Forward
-      </Link>
+      </button>
     </div>
   );
 };
