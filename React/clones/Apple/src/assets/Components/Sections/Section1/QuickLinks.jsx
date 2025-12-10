ff867a4fe9f445e4a54e4ff0307ad4286ae3c1f5
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-10 09:45:37 +0530

    fixed ui for xl screens

--- React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Section1/QuickLinks.jsx
@@ -9,7 +9,7 @@ const Quick = () => {
       <div className="mt-1.5 -translate-x-0.5 sm:mt-1.5 md:mt-[-0.1rem] md:ml-[-0.2rem] lg:mt-2 xl:mt-1">
         <div
           id="card1"
-          className="w-full py-3 pl-6 pr-5.5 flex items-center gap-2 overflow-x-auto whitespace-nowrap will-change-transform sm:tracking-wide"
+          className="w-full py-3 pl-6 pr-5.5 flex items-center gap-2 xl:gap-x-3 overflow-x-auto whitespace-nowrap will-change-transform sm:tracking-wide"
         >
           <span className="text-[0.9rem] lg:text-[0.95rem] md:text-[0.7rem] font-[SFProDisplay] px-[1.1rem] py-[0.45rem] md:py-1.5 md:px-3 lg:px-3.5 border-1 border-black rounded-full">
             Find a Store
