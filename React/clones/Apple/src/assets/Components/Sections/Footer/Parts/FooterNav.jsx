8bb2ae1cb1d29ca3466df399691d5cfe317e18ad
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-10 21:00:15 +0530

    footer nav for sm screen done

--- React/clones/Apple/src/assets/Components/Sections/Footer/Parts/FooterNav.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Footer/Parts/FooterNav.jsx
@@ -5,10 +5,10 @@ const FooterNav = () => {
   return (
     <div className="h-[3.5625rem] bg-amber-400 w-full mt-2 flex items-center gap-x-2 pl-2.5 pt-1">
       <div className="flex p-1 gap-x-1 items-center justify-center">
-        <span className="-translate-y-0.5 text-[1.1rem] ">
+        <span className="-translate-y-0.5 sm:translate-y-0 text-[1.1rem] ">
           <FaApple />
         </span>
-        <IoIosArrowForward className="text-gray-500" />
+        <IoIosArrowForward className="text-gray-500 sm:translate-y-0.5" />
       </div>
       <span className="text-[0.8rem] translate-y-0.5 -translate-x-0.5 text-gray-700 tracking-tight">
         Apple Store Online
