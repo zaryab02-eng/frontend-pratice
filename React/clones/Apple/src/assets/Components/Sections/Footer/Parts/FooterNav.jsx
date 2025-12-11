812fdef45d07996f60983b43bb4a88211bf0327c
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-11 11:47:06 +0530

    yellow bg remove from nav footer

--- React/clones/Apple/src/assets/Components/Sections/Footer/Parts/FooterNav.jsx
+++ React/clones/Apple/src/assets/Components/Sections/Footer/Parts/FooterNav.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowForward } from "react-icons/io";
 
 const FooterNav = () => {
   return (
-    <div className="h-[3.5625rem] bg-amber-400 w-full mt-2 flex items-center md:-translate-y-120 lg:-translate-y-20 xl:-translate-y-44 xl:pl-35 gap-x-2 pl-2.5 pt-1">
+    <div className="h-[3.5625rem] w-full mt-2 flex items-center md:-translate-y-120 lg:-translate-y-20 xl:-translate-y-44 xl:pl-35 gap-x-2 pl-2.5 pt-1">
       <div className="flex p-1 gap-x-1 items-center justify-center lg:pl-2 lg:gap-x-1.5">
         <span className="-translate-y-0.5 sm:translate-y-0 md:-translate-y-1.5 text-[1.1rem] md:text-[0.7rem] lg:text-[1rem] lg:translate-y-0 xl:-translate-y-0.5">
           <FaApple />
