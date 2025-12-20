ad2a93a5f86a313566eab0ddfa780ee40293107a
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-20 19:02:53 +0530

    done

--- React TypeScript/Practice1/03/src/App.tsx
+++ React TypeScript/Practice1/03/src/App.tsx
@@ -16,9 +16,16 @@ const App = () => {
     });
   };
 
+  let display;
+  if (num % 2 == 0) {
+    display = `${num} is even`;
+  } else {
+    display = `${num} is odd`;
+  }
+
   return (
     <div>
-      <div className="px-4 py-2 bg-white text-black w-max">{num}</div>
+      <div className="px-4 py-2 bg-white text-black w-max">{display}</div>
       <div className="flex gap-x-2 mt-1.5">
         <button
           className="px-2 py-0.5 bg-green-500 w-max rounded"
