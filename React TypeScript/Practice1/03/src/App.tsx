5b8d3bae66c0fa7401958f536e33748aa97ecf80
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-20 17:09:07 +0530

    solving

--- React TypeScript/Practice1/03/src/App.tsx
+++ React TypeScript/Practice1/03/src/App.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [num, setNum] = useState(0);
+
+  const inc = () => {
+    setNum((prev) => {
+      return prev + 1;
+    });
+  };
+
+  const dec = () => {
+    setNum((prev) => {
+      if (prev <= 0) return prev;
+      return prev - 1;
+    });
+  };
+
+  return (
+    <div>
+      <div className="px-4 py-2 bg-white text-black w-max">{num}</div>
+      <div className="flex gap-x-2 mt-1.5">
+        <button
+          className="px-2 py-0.5 bg-green-500 w-max rounded"
+          onClick={inc}
+        >
+          INCREASE
+        </button>
+        <button className="px-2 py-0.5 bg-red-500 w-max rounded" onClick={dec}>
+          DECREASE
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
