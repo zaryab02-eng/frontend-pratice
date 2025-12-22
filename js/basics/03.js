356eaa342c4a9f53a7d7378b5440f04f4c36f9d9
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-22 21:01:46 +0530

    solving

--- js/basics/03.js
+++ js/basics/03.js
@@ -1,7 +1,9 @@
-const arr = [1, 2, 3, 4];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-  sum = sum + arr[i];
-}
+const sumArr = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
 
-console.log(sum);
+console.log(sumArr([1, 2, 3, 4]));
