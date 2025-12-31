9f37ca92d225293711e6dd7475a735ceb6176657
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-31 11:52:02 +0530

    solving

---
+++ js/Strings/02.js
@@ -0,0 +1,17 @@
+const countVowels = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countVowels("education"));
