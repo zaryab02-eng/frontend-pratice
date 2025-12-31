d919791744bc51bb31a1ae1ebc943fa148b496e8
zaryab02-eng <mohdzaryab99@gmail.com>
2025-12-31 11:58:00 +0530

    solving

---
+++ js/Strings/03.js
@@ -0,0 +1,17 @@
+const Count = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] !== "a" &&
+      str[i] !== "e" &&
+      str[i] !== "i" &&
+      str[i] !== "o" &&
+      str[i] !== "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(Count("education"));
