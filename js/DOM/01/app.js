8f24984c81a92b8d1e20c07e46ee37be127358af
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-02 15:27:31 +0530

    practicing

--- js/DOM/01/app.js
+++ js/DOM/01/app.js
@@ -3,11 +3,16 @@ const txt = document.getElementById("txt");
 
 let isClicked = false;
 
+txt.innerText = "UnClicked";
+btn.innerText = "Turn ON";
+
 btn.addEventListener("click", () => {
   if (isClicked) {
     txt.innerText = "Clicked";
+    btn.innerText = "Turn OFF";
   } else {
     txt.innerText = "UnClicked";
+    btn.innerText = "Turn ON";
   }
   isClicked = !isClicked;
 });
