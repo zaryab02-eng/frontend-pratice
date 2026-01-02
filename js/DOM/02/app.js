dbe04d41258ffc6e6ee52d7f809c132becfad8a5
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-03 00:30:00 +0530

    practicing

--- js/DOM/02/app.js
+++ js/DOM/02/app.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("btn");
+const text = document.getElementById("text");
 
+text.innerText = "Current Mode: Dark";
 btn.innerText = "☀️ Light Mode";
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
@@ -7,18 +9,20 @@ document.body.style.color = "white";
 let isLight = false;
 
 btn.addEventListener("click", () => {
-  if ((isLight = false)) {
+  if (isLight === false) {
     btn.innerText = "🌙 Dark Mode";
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
     btn.style.backgroundColor = "black";
     btn.style.color = "white";
+    text.innerText = "Current Mode: Light";
   } else {
     btn.innerText = "☀️ Light Mode";
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
     btn.style.backgroundColor = "white";
     btn.style.color = "black";
+    text.innerText = "Current Mode: Dark";
   }
   isLight = !isLight;
 });
