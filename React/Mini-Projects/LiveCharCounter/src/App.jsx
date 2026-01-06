9f4878a14a9fcd16bad056f8d77e0d4986472a29
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-06 21:12:32 +0530

    practicing

--- React/Mini-Projects/LiveCharCounter/src/App.jsx
+++ React/Mini-Projects/LiveCharCounter/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [text, setText] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="h-72 w-52 border border-white flex flex-col justify-center items-center px-28 space-y-3">
+        <h4 id="display" style={{ color: text.length > 20 ? "red" : "white" }}>
+          {text.length > 20
+            ? `Limit Reached ${text.length}`
+            : `Character Length: ${text.length}`}
+        </h4>
+        <input
+          type="text"
+          placeholder="Enter text here"
+          className="bg-amber-100 text-black"
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
