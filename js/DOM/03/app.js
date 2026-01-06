8f31b64f1ddd548674d26685bc9c80129c471306
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-06 14:03:31 +0530

    practiting

--- js/DOM/03/app.js
+++ js/DOM/03/app.js
@@ -1,6 +1,7 @@
 const display = document.getElementById("display");
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+const counter = document.getElementById("counter");
 
 btn.addEventListener("click", () => {
   let inputTxt = input.value;
@@ -10,4 +11,8 @@ btn.addEventListener("click", () => {
     : (display.innerText = inputTxt);
 
   input.value = "";
+});
+
+input.addEventListener("input", () => {
+  counter.innerText = `Characters: ${input.value.length}`;
 });
