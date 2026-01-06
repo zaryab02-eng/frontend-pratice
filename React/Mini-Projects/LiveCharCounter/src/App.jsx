292634f604a99f7325c7c6890888c780ed70c505
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-06 21:28:42 +0530

    working

--- React/Mini-Projects/LiveCharCounter/src/App.jsx
+++ React/Mini-Projects/LiveCharCounter/src/App.jsx
@@ -4,9 +4,11 @@ const App = () => {
   const [text, setText] = useState("");
   return (
     <div>
-      <div className="h-72 w-52 border border-white flex flex-col justify-center items-center px-28 space-y-3">
+      <div className="h-72 w-52 border border-white flex flex-col justify-center items-center space-y-3">
         <h4 id="display" style={{ color: text.length > 20 ? "red" : "white" }}>
-          {text.length > 20
+          {text.length === 0
+            ? `Start Typing..`
+            : text.length > 20
             ? `Limit Reached ${text.length}`
             : `Character Length: ${text.length}`}
         </h4>
@@ -14,8 +16,9 @@ const App = () => {
           type="text"
           value={text}
           placeholder="Enter text here"
-          className="bg-amber-100 text-black"
+          className="bg-amber-100 text-black w-44"
           onChange={(e) => setText(e.target.value)}
+          disabled={text.length > 25}
         />
       </div>
     </div>
