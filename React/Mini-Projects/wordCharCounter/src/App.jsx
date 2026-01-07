b4e526a22d1959bbe99a722f81b425f74585a9d9
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-07 12:54:11 +0530

    refactor: replace temperature display with character counter functionality

--- React/Mini-Projects/wordCharCounter/src/App.jsx
+++ React/Mini-Projects/wordCharCounter/src/App.jsx
@@ -1,42 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  const [temp, setTemp] = useState(20);
+  const [text, setText] = useState("");
 
   return (
-    <>
-      <div className="flex flex-col gap-y-2">
-        <p className="px-2.5 py-2 bg-white rounded text-black">
-          Temperature {temp}°
-        </p>
-        <div className="flex gap-x-0.5">
-          <button
-            className="px-1.5 py-1 bg-green-600 hover:cursor-pointer hover:bg-green-400"
-            onClick={() => setTemp(temp + 1)}
-          >
-            Increase
-          </button>
-          <button
-            className="px-1.5 py-1 bg-red-600 hover:cursor-pointer hover:bg-red-400"
-            onClick={() => setTemp(temp - 1)}
-          >
-            Decrease
-          </button>
-        </div>
-        {temp < 0 && (
-          <p className="text-blue-500 font-semibold text-lg">Freezing!🥶</p>
-        )}
-        {temp >= 0 && temp < 15 && (
-          <p className="text-cyan-600 font-semibold text-lg">Cold!🧥</p>
-        )}
-        {temp >= 15 && temp < 25 && (
-          <p className="text-green-600 font-semibold text-lg">Nice!😊</p>
-        )}
-        {temp >= 25 && (
-          <p className="text-red-500 font-semibold text-lg">Hot!🔥</p>
-        )}
-      </div>
-    </>
+    <div>
+      <h3>{`Character Count: ${text.length}`}</h3>
+      <h4>{text}</h4>
+      <input
+        type="text"
+        value={Text}
+        onChange={(e) => setText(e.target.value)}
+        className="bg-white"
+      />
+    </div>
   );
 };
 
