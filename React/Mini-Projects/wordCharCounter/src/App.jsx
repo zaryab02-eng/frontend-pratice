80ec1b6e09246867cd695bcdf78fc7e650503252
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-07 13:33:35 +0530

    refactor: update word counting logic and improve input styling

--- React/Mini-Projects/wordCharCounter/src/App.jsx
+++ React/Mini-Projects/wordCharCounter/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 const App = () => {
   const [text, setText] = useState("");
+  const countWords = () => {
+    {
+      return text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+    }
+  };
 
   return (
     <div>
       <h3>{`Character Count: ${text.length}`}</h3>
-      <h4>{text}</h4>
+      <h4>{`Words Count: ${countWords()}`}</h4>
       <input
         type="text"
-        value={Text}
+        value={text}
         onChange={(e) => setText(e.target.value)}
-        className="bg-white"
+        className="bg-white text-black px-1 py-1"
+        placeholder="Enter here"
       />
     </div>
   );
