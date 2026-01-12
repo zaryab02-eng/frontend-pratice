de9e700ef97eb2a9d74ce0b5fe1227060dc94c8b
zaryab02-eng <mohdzaryab99@gmail.com>
2026-01-13 01:00:01 +0530

    feat: implement todo list functionality with input handling

--- React/Mini-Projects/todo3/src/App.jsx
+++ React/Mini-Projects/todo3/src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 const App = () => {
+  const [text, setText] = useState("");
+  const [toDo, setToDo] = useState([]);
+
+  const addItem = () => {
+    setToDo([...toDo, text]);
+  };
+
   return (
     <div className="space-y-1">
       <div className="space-x-1">
         <input
           type="text"
+          value={text}
           placeholder="enter here"
           className="bg-white placeholder:text-black text-black"
+          onChange={(e) => setText(e.target.value)}
         />
-        <button className="bg-red-500 text-white">Add</button>
+        <button className="bg-red-500 text-white" onClick={addItem}>
+          Add
+        </button>
       </div>
-      <ul className="w-59.5 h-44 bg-blue-300"></ul>
+      <ul className=" bg-white text-black">
+        {toDo.map((item, idx) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
