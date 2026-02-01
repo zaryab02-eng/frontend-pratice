1cb44bb7cf8113704be4657798246f0c2b7186fa
zaryab02-eng <mohdzaryab99@gmail.com>
2026-02-01 23:28:22 +0530

    working

--- javaScript/basics/app.js
+++ javaScript/basics/app.js
@@ -3,7 +3,12 @@
 // let finalPrice = price + tax;
 // console.log(finalPrice);
 
-const birthYear = 2003;
-let currentYear = 2026;
-let age = currentYear - birthYear;
-console.log(age);
+// const birthYear = 2003;
+// let currentYear = 2026;
+// let age = currentYear - birthYear;
+// console.log(age);
+
+// let username = "Zaryab";
+// let isLoggedIn = true;
+
+// console.log(`Username:${username}, isLoggedIn:${isLoggedIn}`);
