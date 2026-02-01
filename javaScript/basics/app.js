896f4402af3203386c448ce92dc60b714e98f3f1
zaryab02-eng <mohdzaryab99@gmail.com>
2026-02-01 23:24:15 +0530

    working

---
+++ javaScript/basics/app.js
@@ -0,0 +1,9 @@
+// let price = 100;
+// let tax = 18;
+// let finalPrice = price + tax;
+// console.log(finalPrice);
+
+const birthYear = 2003;
+let currentYear = 2026;
+let age = currentYear - birthYear;
+console.log(age);
