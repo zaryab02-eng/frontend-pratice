5fb91538f69579c255547e0016acdf0c437c5399
zaryab02-eng <mohdzaryab99@gmail.com>
2026-02-24 15:25:23 +0530

    solving

---
+++ javaScript/basics/06.js
@@ -0,0 +1,11 @@
+let marks = 92;
+
+if (marks >= 92) {
+  console.log("A");
+} else if (marks >= 75) {
+  console.log("B");
+} else if (marks >= 50) {
+  console.log("C");
+} else {
+  console.log("fail");
+}
