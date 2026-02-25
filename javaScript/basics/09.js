30cc6fe9f833cb090ee6961b52abc89a6c80121e
zaryab02-eng <mohdzaryab99@gmail.com>
2026-02-25 21:16:46 +0530

    solving

---
+++ javaScript/basics/09.js
@@ -0,0 +1,12 @@
+let age = 16;
+let hasID = true;
+
+if (age >= 18) {
+  if (hasID) {
+    console.log("Allowed");
+  } else {
+    console.log("ID Required");
+  }
+} else {
+  console.log("Too Young");
+}
