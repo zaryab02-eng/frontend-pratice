11c6a7c80870c07c1c051a02a06c8fea441507cf
zaryab02-eng <mohdzaryab99@gmail.com>
2026-03-11 01:24:57 +0530

    solving

--- javaScript/basics/30.js
+++ javaScript/basics/30.js
@@ -1,3 +1,10 @@
 let arr = [1, 2, 3, 2, 4, 5, 3];
+let count = 0;
 
-for (let i = 0; i < arr.length; i++) {}
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      console.log(arr[i]);
+    }
+  }
+}
