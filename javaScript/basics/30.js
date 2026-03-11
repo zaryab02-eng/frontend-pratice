b6eb96271f997eef13d555190efcfd11c956f31c
zaryab02-eng <mohdzaryab99@gmail.com>
2026-03-11 22:29:09 +0530

    working

--- javaScript/basics/30.js
+++ javaScript/basics/30.js
@@ -1,10 +1,9 @@
-let arr = [1, 2, 3, 2, 4, 5, 3];
-let count = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] === arr[j]) {
-      console.log(arr[i]);
-    }
-  }
+function sayHello(name) {
+  console.log("Hello " + name);
 }
+
+function greetUser(callback) {
+  callback("Zaryab");
+}
+
+greetUser(sayHello);
