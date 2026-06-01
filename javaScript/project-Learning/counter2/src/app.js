3db473c5285c3d01a68a9af2e18ace56bdb649e8
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-01 21:41:04 +0530

    Add reset button and update color logic in counter functionality

--- javaScript/project-Learning/counter2/src/app.js
+++ javaScript/project-Learning/counter2/src/app.js
@@ -1,15 +1,35 @@
 const display = document.getElementById("disp");
 const incBtn = document.getElementById("inc");
 const decBtn = document.getElementById("dec");
+const reset = document.getElementById("reset");
 
 let count = 0;
 
+function updateColor() {
+  if (count > 0) {
+    display.style.color = "green";
+  } else if (count < 0) {
+    display.style.color = "red";
+  } else {
+    display.style.color = "yellow";
+  }
+  display.innerText = count;
+}
+
 incBtn.addEventListener("click", () => {
   count++;
-  display.innerText = count;
+
+  updateColor();
 });
 
 decBtn.addEventListener("click", () => {
   count--;
-  display.innerText = count;
+
+  updateColor();
+});
+
+reset.addEventListener("click", () => {
+  count = 0;
+
+  updateColor();
 });
