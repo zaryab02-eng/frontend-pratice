7f6e971a3445ff338b1ee75f8d0920ad33ef6a45
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-03 00:39:48 +0530

    Refactor character counter styles and update color logic for better readability

--- javaScript/project-Learning/charLimitCounter/src/app.js
+++ javaScript/project-Learning/charLimitCounter/src/app.js
@@ -4,4 +4,13 @@ const limitCounter = document.getElementById("limitCounter");
 textArea.addEventListener("input", () => {
   let count = textArea.value.length;
   limitCounter.innerText = `${count}/50`;
+
+  if (count === 0) {
+    limitCounter.className = "text-black";
+  } else if (count > 0 && count <= 50) {
+    // Changed >= to >
+    limitCounter.className = "text-green-500";
+  } else {
+    limitCounter.className = "text-red-500";
+  }
 });
