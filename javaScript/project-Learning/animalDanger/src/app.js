2965fd4afe308131e1dcbe7f9ee13571b6114431
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-04 00:15:05 +0530

    Refactor animal data structure to include leg count and update display logic

--- javaScript/project-Learning/animalDanger/src/app.js
+++ javaScript/project-Learning/animalDanger/src/app.js
@@ -2,16 +2,15 @@ const namee = document.getElementById("name");
 const btn = document.getElementById("btn");
 
 const animals = [
-  { name: "Lion", danger: true },
-  { name: "Rabbit", danger: false },
-  { name: "Tiger", danger: true },
-  { name: "Deer", danger: false },
+  { name: "Lion", danger: true, legs: 4 },
+  { name: "Spider", danger: false, legs: 8 },
+  { name: "Rabbit", danger: false, legs: 4 },
 ];
 
 btn.addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * animals.length);
   const animal = animals[randomIndex];
-  namee.innerText = animal.name;
+  namee.innerText = `${animal.name}\n${animal.legs} legs\n ${animal.danger ? `Dangerous` : ` Safe`}`;
 
   if (animal.danger === true) {
     namee.className = "text-red-500";
