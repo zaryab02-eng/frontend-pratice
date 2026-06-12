7175bdb6db47a58c06626d7c5f01cce64e2ff5ad
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-12 18:14:03 +0530

    working

--- javaScript/project-Learning/js/19.js
+++ javaScript/project-Learning/js/19.js
@@ -1,22 +1,41 @@
+// const products = [
+//   { name: "Laptop", stock: 5 },
+//   { name: "Mouse", stock: 10 },
+//   { name: "Keyboard", stock: 8 },
+// ];
+
+// const result = products.reduce(
+//   (current, product) => {
+//     return {
+//       totalStock: current.totalStock + product.stock,
+//       totalProducts: current.totalProducts + 1,
+//     };
+//   },
+//   {
+//     totalStock: 0,
+//     totalProducts: 0,
+//   },
+// );
+
+// result.averageStock = result.totalStock / result.totalProducts;
+
+// console.log(result);
+
 const products = [
   { name: "Laptop", stock: 5 },
   { name: "Mouse", stock: 10 },
   { name: "Keyboard", stock: 8 },
 ];
 
-const result = products.reduce(
-  (current, product) => {
-    return {
-      totalStock: current.totalStock + product.stock,
-      totalProducts: current.totalProducts + 1,
-    };
-  },
-  {
-    totalStock: 0,
-    totalProducts: 0,
-  },
-);
+const winner = products.reduce((current, product) => {
+  if (product.stock > current.stock) {
+    return product;
+  }
+  return current;
+}, products[0]);
 
-result.averageStock = result.totalStock / result.totalProducts;
+const result = {
+  mostStock: winner,
+};
 
 console.log(result);
