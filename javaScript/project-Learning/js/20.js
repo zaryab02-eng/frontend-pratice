2a02c42364b1db77af2c3b3983b32ab1ab1d2797
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-13 15:19:19 +0530

    working

--- javaScript/project-Learning/js/20.js
+++ javaScript/project-Learning/js/20.js
@@ -103,6 +103,44 @@
 
 // console.log(winner);
 
+// const products = [
+//   { name: "Laptop", stock: 5, price: 50000 },
+//   { name: "Mouse", stock: 10, price: 1000 },
+//   { name: "Keyboard", stock: 8, price: 2000 },
+// ];
+
+// const winner = products.reduce(
+//   (current, product) => {
+//     return {
+//       totalPrice: product.price + current.totalPrice,
+//       totalStock: product.stock + current.totalStock,
+//       cheapestProduct:
+//         product.price < current.cheapestProduct.price
+//           ? product
+//           : current.cheapestProduct,
+//       mostStockProduct:
+//         product.stock > current.mostStockProduct.stock
+//           ? product
+//           : current.mostStockProduct,
+//       highestPriceProduct:
+//         product.price > current.highestPriceProduct.price
+//           ? product
+//           : current.highestPriceProduct,
+//     };
+//   },
+//   {
+//     totalPrice: 0,
+//     totalStock: 0,
+//     cheapestProduct: products[0],
+//     mostStockProduct: products[0],
+//     highestPriceProduct: products[0],
+//   },
+// );
+
+// winner.averagePrice = winner.totalPrice / products.length;
+
+// console.log(winner);
+
 const products = [
   { name: "Laptop", stock: 5, price: 50000 },
   { name: "Mouse", stock: 10, price: 1000 },
@@ -113,15 +151,12 @@ const winner = products.reduce(
   (current, product) => {
     return {
       totalPrice: product.price + current.totalPrice,
-      totalStock: product.stock + current.totalStock,
-      cheapestProduct:
-        product.price < current.cheapestProduct.price
-          ? product
-          : current.cheapestProduct,
-      mostStockProduct:
-        product.stock > current.mostStockProduct.stock
-          ? product
-          : current.mostStockProduct,
+      inStockCount:
+        product.stock > 0 ? current.inStockCount + 1 : current.inStockCount,
+      outOfStockCount:
+        product.stock === 0
+          ? current.outOfStockCount + 1
+          : current.outOfStockCount,
       highestPriceProduct:
         product.price > current.highestPriceProduct.price
           ? product
@@ -130,13 +165,10 @@ const winner = products.reduce(
   },
   {
     totalPrice: 0,
-    totalStock: 0,
-    cheapestProduct: products[0],
-    mostStockProduct: products[0],
+    inStockCount: 0,
+    outOfStockCount: 0,
     highestPriceProduct: products[0],
   },
 );
-
-winner.averagePrice = winner.totalPrice / products.length;
 
 console.log(winner);
