d61c0e6839a4cff5d199e5105914f1d6f85e15df
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-13 11:48:10 +0530

    solving

--- javaScript/project-Learning/js/20.js
+++ javaScript/project-Learning/js/20.js
@@ -46,6 +46,32 @@
 
 // console.log(winner);
 
+// const products = [
+//   { name: "Laptop", stock: 5, price: 50000 },
+//   { name: "Mouse", stock: 10, price: 1000 },
+//   { name: "Keyboard", stock: 8, price: 2000 },
+// ];
+
+// const winner = products.reduce(
+//   (current, product) => {
+//     return {
+//       totalPrice: product.price + current.totalPrice,
+//       totalStock: product.stock + current.totalStock,
+//       cheapestProduct:
+//         product.price < current.cheapestProduct.price
+//           ? product
+//           : current.cheapestProduct,
+//     };
+//   },
+//   {
+//     totalPrice: 0,
+//     totalStock: 0,
+//     cheapestProduct: products[0],
+//   },
+// );
+
+// console.log(winner);
+
 const products = [
   { name: "Laptop", stock: 5, price: 50000 },
   { name: "Mouse", stock: 10, price: 1000 },
@@ -61,12 +87,17 @@ const winner = products.reduce(
         product.price < current.cheapestProduct.price
           ? product
           : current.cheapestProduct,
+      mostStockProduct:
+        product.stock > current.mostStockProduct.stock
+          ? product
+          : current.mostStockProduct,
     };
   },
   {
     totalPrice: 0,
     totalStock: 0,
     cheapestProduct: products[0],
+    mostStockProduct: products[0],
   },
 );
 
