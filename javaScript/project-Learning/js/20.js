ad82308e735eef1de45fee540fa2881e7a0e6623
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-13 12:15:01 +0530

    solving

--- javaScript/project-Learning/js/20.js
+++ javaScript/project-Learning/js/20.js
@@ -72,6 +72,37 @@
 
 // console.log(winner);
 
+// const products = [
+//   { name: "Laptop", stock: 5, price: 50000 },
+//   { name: "Mouse", stock: 10, price: 1000 },
+//   { name: "Keyboard", stock: 8, price: 2000 },
+// ];
+
+// const winner = products.reduce(
+//   (current, product) => {
+//     return {
+//       totalPrice: product.price + current.totalPrice,
+//       totalStock: product.stock + current.totalStock,
+//       cheapestProduct:
+//         product.price < current.cheapestProduct.price
+//           ? product
+//           : current.cheapestProduct,
+//       mostStockProduct:
+//         product.stock > current.mostStockProduct.stock
+//           ? product
+//           : current.mostStockProduct,
+//     };
+//   },
+//   {
+//     totalPrice: 0,
+//     totalStock: 0,
+//     cheapestProduct: products[0],
+//     mostStockProduct: products[0],
+//   },
+// );
+
+// console.log(winner);
+
 const products = [
   { name: "Laptop", stock: 5, price: 50000 },
   { name: "Mouse", stock: 10, price: 1000 },
@@ -91,6 +122,10 @@ const winner = products.reduce(
         product.stock > current.mostStockProduct.stock
           ? product
           : current.mostStockProduct,
+      highestPriceProduct:
+        product.price > current.highestPriceProduct.price
+          ? product
+          : current.highestPriceProduct,
     };
   },
   {
@@ -98,7 +133,10 @@ const winner = products.reduce(
     totalStock: 0,
     cheapestProduct: products[0],
     mostStockProduct: products[0],
+    highestPriceProduct: products[0],
   },
 );
+
+winner.averagePrice = winner.totalPrice / products.length;
 
 console.log(winner);
