3306678c4800b8a989bd9e088212a59fca498778
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-15 15:25:15 +0530

    solving

---
+++ javaScript/project-Learning/js/24.js
@@ -0,0 +1,34 @@
+// const fruits = ["Apple", "Mango", "Banana"];
+
+// const [firstFruit, secondFruit, thirdFruit] = fruits;
+
+// console.log(firstFruit);
+// console.log(secondFruit);
+// console.log(thirdFruit);
+
+// const cities = ["Delhi", "Mumbai", "Lucknow", "Pune"];
+
+// const [firstCity, , , fourthCity] = cities;
+// console.log(firstCity, fourthCity);
+
+// const user = {
+//   name: "Zaryab",
+//   age: 22,
+//   city: "Prayagraj",
+// };
+
+// function showUsers({ name: userName, age: userAge, city: userCity } = user) {
+//   return `${userName} from ${userCity}`;
+// }
+
+// console.log(showUsers());
+
+// const greet = (name) => {
+//   return `Hello ${name}`;
+// };
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(2, 2));
