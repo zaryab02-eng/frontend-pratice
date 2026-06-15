fecdf27d2f290e1a8457be40039d4f649c505a7f
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-15 14:55:42 +0530

    solving

--- javaScript/project-Learning/js/23.js
+++ javaScript/project-Learning/js/23.js
@@ -19,3 +19,33 @@ const student = {
 };
 
 const { name: studentName, marks: studentMarks } = student;
+
+const movie = {
+  title: "Inception",
+  rating: 8.8,
+  year: 2010,
+  director: "Christopher Nolan",
+};
+
+const {
+  title: mmovieTitle,
+  rating: movieRating,
+  director: movieDirector,
+} = movie;
+
+const user = {
+  name: "Zaryab",
+  age: 22,
+};
+
+const { name: userName, age: userAge } = user;
+console.log(`${userName} is ${userAge} year old`);
+
+const admin = {
+  name: "Zaryab",
+  email: "zaryab@gmail.com",
+  role: "Admin",
+};
+
+const { name: adminName, email: adminMail, role: roleAdmin } = admin;
+console.log(`${roleAdmin}: ${adminName} | ${adminMail} | ${roleAdmin}`);
