75616ccca3515fa8da51685d3c0845bad2b159d3
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-15 12:10:46 +0530

    solving

---
+++ javaScript/project-Learning/js/23.js
@@ -0,0 +1,21 @@
+const product = {
+  name: "iPhone 15",
+  price: 79999,
+  stock: 12,
+  rating: 4.8,
+};
+
+const {
+  name: productName,
+  price: productPrice,
+  stock: productStock,
+  rating: productRating,
+} = product;
+
+const student = {
+  name: "Areeba",
+  marks: 89,
+  city: "Delhi",
+};
+
+const { name: studentName, marks: studentMarks } = student;
