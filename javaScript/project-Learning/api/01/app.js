29318bf5672507af3e778a229e78894135bede94
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-17 21:53:51 +0530

    workinf

--- javaScript/project-Learning/api/01/app.js
+++ javaScript/project-Learning/api/01/app.js
@@ -21,11 +21,60 @@
 //     // Print only their names
 //   });
 
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const userComanyNameGroup = data.filter(({ company: { name } }) =>
+//       name.includes("Group"),
+//     );
+//     console.log(userComanyNameGroup.length);
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const {
+//       name,
+//       company: { name: companyName },
+//       address: { city },
+//     } = data.find(({ id }) => id === 5);
+//     console.log({ name, companyName, city });
+
+//     // Find the user whose id is 5
+//     //
+//     // Print:
+//     // {
+//     //   name,
+//     //   companyName,
+//     //   city
+//     // }
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const newArr = data.map(({ name, address: { city } }) => {
+//       return (name, city);
+//     });
+//     console.log(newArr);
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const newArr = data.map(({ name, email, address: { city } }) => {
+//       return { name, email, city };
+//     });
+//     console.log(newArr);
+//   });
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
-    const userComanyNameGroup = data.filter(({ company: { name } }) =>
-      name.includes("Group"),
-    );
-    console.log(userComanyNameGroup.length);
+    const newArr = data
+      .filter(({ id }) => id > 5)
+      .map(({ name, company: { name: companyName } }) => {
+        return { name, companyName };
+      });
+    console.log(newArr);
   });
