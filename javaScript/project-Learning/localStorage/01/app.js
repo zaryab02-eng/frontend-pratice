96dc9aadcb153ef9e12906bd702e3799fc81762d
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-17 12:04:34 +0530

    local storage done

--- javaScript/project-Learning/localStorage/01/app.js
+++ javaScript/project-Learning/localStorage/01/app.js
@@ -24,14 +24,40 @@
 // const objData = JSON.parse(userData);
 // console.log(`${objData.name} from ${objData.city}`);
 
-const cart = [
-  { name: "Laptop", price: 65000 },
-  { name: "Mouse", price: 500 },
-  { name: "Keyboard", price: 1500 },
+// const cart = [
+//   { name: "Laptop", price: 65000 },
+//   { name: "Mouse", price: 500 },
+//   { name: "Keyboard", price: 1500 },
+// ];
+
+// localStorage.setItem("cart", JSON.stringify(cart));
+// const data = localStorage.getItem("cart");
+// const objData = JSON.parse(data);
+// const totalPrice = objData.reduce((current, { price }) => current + price, 0);
+// console.log(totalPrice);
+
+// const movies = [
+//   { title: "Inception", rating: 8.8 },
+//   { title: "Interstellar", rating: 8.6 },
+//   { title: "Batman Begins", rating: 8.2 },
+// ];
+
+// localStorage.setItem("movies", JSON.stringify(movies));
+// const data = localStorage.getItem("movies");
+// const objData = JSON.parse(data);
+// const newArr = objData.map(({ title }) => title);
+// console.log(newArr);
+
+const todos = [
+  { text: "Learn JS", completed: true },
+  { text: "Build Project", completed: false },
+  { text: "Learn React", completed: false },
 ];
 
-localStorage.setItem("cart", JSON.stringify(cart));
-const data = localStorage.getItem("cart");
+localStorage.setItem("todos", JSON.stringify(todos));
+const data = localStorage.getItem("todos");
 const objData = JSON.parse(data);
-const totalPrice = objData.reduce((current, { price }) => current + price, 0);
-console.log(totalPrice);
+const incompleteTodos = objData
+  .filter(({ completed }) => completed === false)
+  .map(({ text }) => text);
+console.log(incompleteTodos);
