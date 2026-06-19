2267069f0c6963462857d88baf03bcf9baced7d6
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-19 16:22:51 +0530

    solving

--- javaScript/project-Learning/api/01/app.js
+++ javaScript/project-Learning/api/01/app.js
@@ -68,13 +68,141 @@
 //     console.log(newArr);
 //   });
 
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const newArr = data
+//       .filter(({ id }) => id > 5)
+//       .map(({ name, company: { name: companyName } }) => {
+//         return { name, companyName };
+//       });
+//     console.log(newArr);
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const {
+//       name,
+//       email,
+//       address: { city },
+//       company: { name: companyName },
+//     } = data.find(({ id }) => id === 3);
+//     console.log({ name, email, city, companyName });
+
+//     // Find the user with id 3
+//     //
+//     // Create:
+//     //
+//     // {
+//     //   name,
+//     //   email,
+//     //   city,
+//     //   companyName
+//     // }
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const newArr = data
+//       .filter(({ address: { city } }) => city.includes("South"))
+//       .map(({ name, address: { city } }) => {
+//         return { name, city };
+//       });
+//     console.log(newArr);
+
+//     // Create an array of objects:
+//     //
+//     // {
+//     //   name,
+//     //   city
+//     // }
+//     //
+//     // Only for users whose city contains "South"
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const {
+//       name,
+//       email,
+//       address: { city },
+//     } = data.find(({ address: { city } }) => city.includes("South"));
+//     console.log({ name, email, city });
+
+//     // Find the first user whose city contains "South"
+//     //
+//     // Print:
+//     //
+//     // {
+//     //   name,
+//     //   email,
+//     //   city
+//     // }
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const userLivesInCityNameSouth = data.filter(({ address: { city } }) =>
+//       city.includes("South"),
+//     );
+//     console.log(userLivesInCityNameSouth.length);
+
+//     // Count how many users live in cities
+//     // containing "South"
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const newArr = data
+//       .filter(({ id }) => id > 5)
+//       .map(({ name, address: { city }, company: { name: companyName } }) => {
+//         return {
+//           name,
+//           city,
+//           companyName,
+//         };
+//       });
+//     console.log(newArr);
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const CityUsers = data.reduce(
+//       (current, { address: { city } }) => {
+//         if (city.includes("South")) {
+//           current.southCityUsers++;
+//         }
+//         return current;
+//       },
+//       {
+//         totalUsers: data.length,
+//         southCityUsers: 0,
+//       },
+//     );
+//     console.log(CityUsers);
+//   });
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
-    const newArr = data
-      .filter(({ id }) => id > 5)
-      .map(({ name, company: { name: companyName } }) => {
-        return { name, companyName };
-      });
-    console.log(newArr);
+    const userCount = data.reduce(
+      (current, { id }) => {
+        return {
+          totalUsers: current.totalUsers + 1,
+          idsGreaterThan5:
+            id > 5 ? current.idsGreaterThan5 + 1 : current.idsGreaterThan5,
+        };
+      },
+      {
+        totalUsers: 0,
+        idsGreaterThan5: 0,
+      },
+    );
+    console.log(userCount);
   });
