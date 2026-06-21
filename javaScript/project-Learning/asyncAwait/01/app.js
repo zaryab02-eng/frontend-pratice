93bc94491b5b2199dafeb9eacc39dac0336e132e
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-21 05:45:08 +0530

    solving

--- javaScript/project-Learning/asyncAwait/01/app.js
+++ javaScript/project-Learning/asyncAwait/01/app.js
@@ -1,22 +1,82 @@
-// async function getPosts() {
-//   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   const data = await response.json();
-//   console.log(data);
+// async function getPost() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch");
+//     }
+//     const data = await response.json();
+//     const southUsers = data.filter(({ address: { city } }) =>
+//       city.includes("South"),
+//     );
+
+//     console.log(southUsers);
+//   } catch (error) {
+//     console.log(error);
+//   }
 // }
 
-// getPosts();
+// getPost();
 
-async function getPosts() {
+// async function userData() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch");
+//     }
+//     const data = await response.json();
+//     const names = data.map(({ name }) => name);
+
+//     console.log(names);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// userData();
+
+// async function userData() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch");
+//     }
+//     const data = await response.json();
+//     const userCount = data.reduce(
+//       (current, { address: { city } }) => {
+//         return {
+//           totalUsers: current.totalUsers + 1,
+//           southCityUsers: city.includes("South")
+//             ? current.southCityUsers + 1
+//             : current.southCityUsers,
+//         };
+//       },
+//       {
+//         totalUsers: 0,
+//         southCityUsers: 0,
+//       },
+//     );
+//     console.log(userCount);
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     console.log("Request Completed");
+//   }
+// }
+
+// userData();
+
+async function userData() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!response.ok) {
       throw new Error("Failed to fetch");
     }
     const data = await response.json();
-    console.log(data);
+    const { name, email } = data.find(({ id }) => id === 8);
+    console.log({ name, email });
   } catch (error) {
     console.log(error);
   }
 }
 
-getPosts();
+userData();
