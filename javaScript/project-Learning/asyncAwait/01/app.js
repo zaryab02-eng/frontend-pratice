b4f134bddf3241701651ff0e127eaa1ece97ec07
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-23 12:34:27 +0530

    solving

--- javaScript/project-Learning/asyncAwait/01/app.js
+++ javaScript/project-Learning/asyncAwait/01/app.js
@@ -1,88 +1,23 @@
-// async function getPost() {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch");
-//     }
-//     const data = await response.json();
-//     const southUsers = data.filter(({ address: { city } }) =>
-//       city.includes("South"),
-//     );
-
-//     console.log(southUsers);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getPost();
-
-// async function userData() {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch");
-//     }
-//     const data = await response.json();
-//     const names = data.map(({ name }) => name);
-
-//     console.log(names);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// userData();
-
-// async function userData() {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch");
-//     }
-//     const data = await response.json();
-//     const userCount = data.reduce(
-//       (current, { address: { city } }) => {
-//         return {
-//           totalUsers: current.totalUsers + 1,
-//           southCityUsers: city.includes("South")
-//             ? current.southCityUsers + 1
-//             : current.southCityUsers,
-//         };
-//       },
-//       {
-//         totalUsers: 0,
-//         southCityUsers: 0,
-//       },
-//     );
-//     console.log(userCount);
-//   } catch (error) {
-//     console.log(error);
-//   } finally {
-//     console.log("Request Completed");
-//   }
-// }
-
-// userData();
-
-async function userData() {
+async function userInfo() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!response.ok) {
       throw new Error("Failed to fetch");
     }
     const data = await response.json();
-    const {
-      name,
-      address: { city },
-      company: { name: copmanyName },
-    } = data.find(({ id }) => id === 7);
-    console.log({ name, city, copmanyName });
+    const user = data.map(
+      ({ name, address: { city }, company: { name: companyName } }) => {
+        return {
+          name,
+          city,
+          companyName,
+        };
+      },
+    );
+    console.log(user);
   } catch (error) {
     console.log(error);
-  } finally {
-    console.log("Successful");
   }
 }
 
-userData();
+userInfo();
