4e5720b33948582637717eeac1d7212bcaefd577
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-24 14:21:54 +0530

    solvinf

--- javaScript/project-Learning/asyncAwait/01/app.js
+++ javaScript/project-Learning/asyncAwait/01/app.js
@@ -5,35 +5,37 @@ async function userInfo() {
       throw new Error("Failed to fetch");
     }
     const data = await response.json();
-    const usersData = data.reduce(
+    const userData = data.reduce(
       (current, user) => {
         const {
           address: { city },
+          name,
         } = user;
 
         if (city.includes("South") && !current.firstSouthUser) {
           current.firstSouthUser = user;
         }
+
         if (city.includes("South")) {
           current.lastSouthUser = user;
+          current.southUserNames.push(name);
         }
+
         return {
           totalUsers: current.totalUsers + 1,
-          southCityUsers: city.includes("South")
-            ? current.southCityUsers + 1
-            : current.southCityUsers,
           firstSouthUser: current.firstSouthUser,
           lastSouthUser: current.lastSouthUser,
+          southUserNames: current.southUserNames,
         };
       },
       {
         totalUsers: 0,
-        southCityUsers: 0,
         firstSouthUser: null,
         lastSouthUser: null,
+        southUserNames: [],
       },
     );
-    console.log(usersData);
+    console.log(userData);
   } catch (error) {
     console.log(error);
   }
