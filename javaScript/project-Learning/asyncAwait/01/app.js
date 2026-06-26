624a4cee6c9941cdd62dc86c7a97c2318b224858
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-26 17:00:42 +0530

    solving

--- javaScript/project-Learning/asyncAwait/01/app.js
+++ javaScript/project-Learning/asyncAwait/01/app.js
@@ -1,50 +1,38 @@
 async function userInfo() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) {
-      throw new Error("Failed to fetch");
-    }
-    const data = await response.json();
-    const userData = data.reduce(
-      (current, user) => {
-        const {
-          name,
-          address: { city },
-        } = user;
-
-        if (city.includes("South") && !current.firstSouthUser) {
-          current.firstSouthUser = user;
-        }
-
-        if (city.includes("South")) {
-          current.lastSouthUser = user;
-          current.southUserNames.push(name);
-          current.southUserCount += 1;
-          current.southUsers.push({ name, city });
-        }
-
-        return {
-          totalUsers: current.totalUsers + 1,
-          firstSouthUser: current.firstSouthUser,
-          lastSouthUser: current.lastSouthUser,
-          southUserNames: current.southUserNames,
-          southUserCount: current.southUserCount,
-          southUsers: current.southUsers,
-        };
-      },
-      {
-        totalUsers: 0,
-        firstSouthUser: null,
-        lastSouthUser: null,
-        southUserNames: [],
-        southUserCount: 0,
-        southUsers: [],
-      },
+    const responseNames = await fetch(
+      "https://jsonplaceholder.typicode.com/users",
     );
-    console.log(userData);
+    const responsePosts = await fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+
+    if (!responseNames.ok) {
+      throw new Error("Failed to fetch user names");
+    }
+    if (!responsePosts.ok) {
+      throw new Error("Failed to fetch user names");
+    }
+
+    const users = await responseNames.json();
+    const posts = await responsePosts.json();
+
+    const result = users.map(({ id, name }) => {
+      const userPosts = posts.filter(({ userId }) => userId === id);
+      return {
+        userName: name,
+        postCount: userPosts.length,
+      };
+    });
+    return result;
   } catch (error) {
     console.log(error);
   }
 }
 
-userInfo();
+async function main() {
+  const user = await userInfo();
+  console.log(user);
+}
+
+main();
