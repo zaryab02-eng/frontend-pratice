a40103fd529405376a73d60121ce2abcbbc38e45
zaryab02-eng <mohdzaryab99@gmail.com>
2026-06-30 16:45:00 +0530

    solving

---
+++ javaScript/project-Learning/asyncAwait/02/app.js
@@ -0,0 +1,79 @@
+// async function userInfo() {
+//   try {
+//     const userResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/users",
+//     );
+//     const postResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/posts",
+//     );
+
+//     if (!userResponse.ok) {
+//       throw new Error("Failed to fetch users");
+//     }
+//     if (!postResponse.ok) {
+//       throw new Error("Failed to fetch posts");
+//     }
+//     const users = await userResponse.json();
+//     const posts = await postResponse.json();
+
+//     const result = users.map(({ id, username }) => {
+//       const matchedPosts = posts.filter(({ userId }) => id === userId);
+//       const hasPosts = posts.some(({ userId }) => id === userId);
+//       const latestPost = matchedPosts[matchedPosts.length - 1];
+//       const { title = null } = latestPost ?? {};
+//       return {
+//         userName: username,
+//         totalPosts: matchedPosts.length,
+//         latestPostTitle: title,
+//         hasPosts: hasPosts,
+//       };
+//     });
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// userInfo();
+
+async function userInfo() {
+  try {
+    const userResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    const postResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+
+    if (!userResponse.ok) {
+      throw new Error("Failed to fetch users");
+    }
+    if (!postResponse.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const users = await userResponse.json();
+    const posts = await postResponse.json();
+
+    const result = users.map(({ id, username }) => {
+      const matchedPost = posts.filter(({ userId }) => id === userId);
+      let longestPostTitle = null;
+      for (const { title } of matchedPost) {
+        if (
+          longestPostTitle === null ||
+          title.length > longestPostTitle.length
+        ) {
+          longestPostTitle = title;
+        }
+      }
+      return {
+        userName: username,
+        longestPostTitle: longestPostTitle,
+      };
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+userInfo();
