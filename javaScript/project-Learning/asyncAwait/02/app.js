32ce13d28813a43aab750447650b0c3967c257f1
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-01 01:03:14 +0530

    solving

--- javaScript/project-Learning/asyncAwait/02/app.js
+++ javaScript/project-Learning/asyncAwait/02/app.js
@@ -36,6 +36,48 @@
 
 // userInfo();
 
+// async function userInfo() {
+//   try {
+//     const userResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/users",
+//     );
+//     const postResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/posts",
+//     );
+
+//     if (!userResponse.ok) {
+//       throw new Error("Failed to fetch users");
+//     }
+//     if (!postResponse.ok) {
+//       throw new Error("Failed to fetch posts");
+//     }
+//     const users = await userResponse.json();
+//     const posts = await postResponse.json();
+
+//     const result = users.map(({ id, username }) => {
+//       const matchedPost = posts.filter(({ userId }) => id === userId);
+//       let longestPostTitle = null;
+//       for (const { title } of matchedPost) {
+//         if (
+//           longestPostTitle === null ||
+//           title.length > longestPostTitle.length
+//         ) {
+//           longestPostTitle = title;
+//         }
+//       }
+//       return {
+//         userName: username,
+//         longestPostTitle: longestPostTitle,
+//       };
+//     });
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// userInfo();
+
 async function userInfo() {
   try {
     const userResponse = await fetch(
@@ -55,19 +97,20 @@ async function userInfo() {
     const posts = await postResponse.json();
 
     const result = users.map(({ id, username }) => {
-      const matchedPost = posts.filter(({ userId }) => id === userId);
-      let longestPostTitle = null;
-      for (const { title } of matchedPost) {
-        if (
-          longestPostTitle === null ||
-          title.length > longestPostTitle.length
-        ) {
-          longestPostTitle = title;
-        }
-      }
+      const postCount = posts.filter(({ userId }) => id === userId);
+      const titles = postCount.map(({ title }) => title);
+      const totalChar = titles.reduce(
+        (total, title) => total + title.length,
+        0,
+      );
+      const averageTitleLength =
+        postCount.length === 0
+          ? 0
+          : Number((totalChar / postCount.length).toFixed(1));
       return {
         userName: username,
-        longestPostTitle: longestPostTitle,
+        postCount: postCount.length,
+        averageTitleLength: averageTitleLength,
       };
     });
     console.log(result);
