c6978ec4a9a4f4f8600dcfd19e5a7c0a313e3f1f
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-10 10:55:47 +0530

    solving

--- javaScript/project-Learning/asyncAwait/02/app.js
+++ javaScript/project-Learning/asyncAwait/02/app.js
@@ -1,42 +1,56 @@
-async function usersInfo() {
-  try {
-    const userResponse = await fetch(
-      "https://jsonplaceholder.typicode.com/users",
-    );
-    const postResponse = await fetch(
-      "https://jsonplaceholder.typicode.com/posts",
-    );
-    if (!userResponse.ok) {
-      throw new Error("Failed to fetch users");
+// async function usersInfo() {
+//   try {
+//     const userResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/users",
+//     );
+//     const postResponse = await fetch(
+//       "https://jsonplaceholder.typicode.com/posts",
+//     );
+//     if (!userResponse.ok) {
+//       throw new Error("Failed to fetch users");
+//     }
+//     if (!postResponse.ok) {
+//       throw new Error("Failed to fetch posts");
+//     }
+//     const users = await userResponse.json();
+//     const posts = await postResponse.json();
+//     if (users.length === 0 || posts.length === 0) {
+//       throw new Error("No data found!");
+//     }
+//     const result = users.map(({ id, name }) => {
+//       const matchedPosts = posts.filter(({ userId }) => id === userId);
+//       let shortestPostBody = null;
+//       for (const { body } of matchedPosts) {
+//         if (shortestPostBody === null) {
+//           shortestPostBody = body;
+//         } else if (body.length < shortestPostBody.length) {
+//           shortestPostBody = body;
+//         }
+//       }
+//       return {
+//         name,
+//         shortestPostBody: shortestPostBody.length,
+//       };
+//     });
+//     result.sort((a, b) => a.shortestPostBody - b.shortestPostBody);
+//     console.log(`${result[0].name} - ${result[0].shortestPostBody}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// usersInfo();
+
+function runningMaximum(num) {
+  let result = [];
+  let largest = num[0];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] > largest) {
+      largest = num[i];
     }
-    if (!postResponse.ok) {
-      throw new Error("Failed to fetch posts");
-    }
-    const users = await userResponse.json();
-    const posts = await postResponse.json();
-    if (users.length === 0 || posts.length === 0) {
-      throw new Error("No data found!");
-    }
-    const result = users.map(({ id, name }) => {
-      const matchedPosts = posts.filter(({ userId }) => id === userId);
-      let shortestPostBody = null;
-      for (const { body } of matchedPosts) {
-        if (shortestPostBody === null) {
-          shortestPostBody = body;
-        } else if (body.length < shortestPostBody.length) {
-          shortestPostBody = body;
-        }
-      }
-      return {
-        name,
-        shortestPostBody: shortestPostBody.length,
-      };
-    });
-    result.sort((a, b) => a.shortestPostBody - b.shortestPostBody);
-    console.log(`${result[0].name} - ${result[0].shortestPostBody}`);
-  } catch (error) {
-    console.log(error);
+    result.push(largest);
   }
+  return result;
 }
 
-usersInfo();
+console.log(runningMaximum([3, 8, 2, 10, 5]));
