dac67f964345facdb5c5ba62911db2f28a6edc10
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-10 23:54:49 +0530

    solving

--- javaScript/project-Learning/asyncAwait/02/app.js
+++ javaScript/project-Learning/asyncAwait/02/app.js
@@ -1,56 +1,40 @@
-// async function usersInfo() {
-//   try {
-//     const userResponse = await fetch(
-//       "https://jsonplaceholder.typicode.com/users",
-//     );
-//     const postResponse = await fetch(
-//       "https://jsonplaceholder.typicode.com/posts",
-//     );
-//     if (!userResponse.ok) {
-//       throw new Error("Failed to fetch users");
-//     }
-//     if (!postResponse.ok) {
-//       throw new Error("Failed to fetch posts");
-//     }
-//     const users = await userResponse.json();
-//     const posts = await postResponse.json();
-//     if (users.length === 0 || posts.length === 0) {
-//       throw new Error("No data found!");
-//     }
-//     const result = users.map(({ id, name }) => {
-//       const matchedPosts = posts.filter(({ userId }) => id === userId);
-//       let shortestPostBody = null;
-//       for (const { body } of matchedPosts) {
-//         if (shortestPostBody === null) {
-//           shortestPostBody = body;
-//         } else if (body.length < shortestPostBody.length) {
-//           shortestPostBody = body;
-//         }
-//       }
-//       return {
-//         name,
-//         shortestPostBody: shortestPostBody.length,
-//       };
-//     });
-//     result.sort((a, b) => a.shortestPostBody - b.shortestPostBody);
-//     console.log(`${result[0].name} - ${result[0].shortestPostBody}`);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// usersInfo();
-
-function runningMinimum(num) {
-  let result = [];
-  let minimum = num[0];
-  for (let i = 0; i < num.length; i++) {
-    if (num[i] < minimum) {
-      minimum = num[i];
+async function usersInfo() {
+  try {
+    const userResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    const postResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    if (!userResponse.ok) {
+      throw new Error("Failed to fetch users");
     }
-    result.push(minimum);
+    if (!postResponse.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const users = await userResponse.json();
+    const posts = await postResponse.json();
+    if (users.length === 0 || posts.length === 0) {
+      throw new Error("No data found!");
+    }
+    const result = users.map(({ id, name }) => {
+      const matchedPost = posts.filter(({ userId }) => id === userId);
+      let count = 0;
+      for (const { body } of matchedPost) {
+        if (body.includes("et")) {
+          count++;
+        }
+      }
+      return {
+        name,
+        count,
+      };
+    });
+    result.sort((a, b) => b.count - a.count);
+    console.log(`${result[0].name} - ${result[0].count}`);
+  } catch (error) {
+    console.log(error);
   }
-  return result;
 }
 
-console.log(runningMinimum([5, 3, 8, 2, 4]));
+usersInfo();
