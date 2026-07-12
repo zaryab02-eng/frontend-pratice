c82ebd2527afc8742cc1d430690e561409e6f9f6
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-12 10:32:57 +0530

    coding

--- javaScript/project-Learning/asyncAwait/02/app.js
+++ javaScript/project-Learning/asyncAwait/02/app.js
@@ -39,14 +39,50 @@
 
 // usersInfo();
 
-function runningAverage(num) {
-  let sum = 0;
-  let result = [];
-  for (let i = 0; i < num.length; i++) {
-    sum += num[i];
-    result.push(sum);
+async function usersInfo() {
+  try {
+    const userResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    const postResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    if (!userResponse.ok) {
+      throw new Error("Failed to fetch users");
+    }
+    if (!postResponse.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const users = await userResponse.json();
+    const posts = await postResponse.json();
+    if (users.length === 0 || posts.length === 0) {
+      throw new Error("No data found");
+    }
+
+    const filteredUsers = users.filter(({ id }) => {
+      const matchedPosts = posts.filter(({ userId }) => id === userId);
+      return matchedPosts.length > 0;
+    });
+
+    const result = filteredUsers.map(({ id, name }) => {
+      const matchedPosts = posts.filter(({ userId }) => id === userId);
+      const totalTitleLength = matchedPosts.reduce(
+        (current, { title }) => current + title.length,
+        0,
+      );
+      const avgTitleLength = Number(
+        (totalTitleLength / matchedPosts.length).toFixed(1),
+      );
+      return {
+        name,
+        avgTitleLength,
+      };
+    });
+    result.sort((a, b) => b.avgTitleLength - a.avgTitleLength);
+    console.log(`${result[0].name} - ${result[0].avgTitleLength}`);
+  } catch (error) {
+    console.log(error);
   }
-  return result;
 }
 
-console.log(runningAverage([2, 4, 6, 8]));
+usersInfo();
