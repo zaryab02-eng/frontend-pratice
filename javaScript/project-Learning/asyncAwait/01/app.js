66a3c6f9481f1f1fa6e3499165be19d76fe73963
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-21 14:56:31 +0530

    studying

--- javaScript/project-Learning/asyncAwait/01/app.js
+++ javaScript/project-Learning/asyncAwait/01/app.js
@@ -208,6 +208,9 @@
 
 // userInfo();
 
-async function userInfo() {}
-
-userInfo();
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data.length))
+  .catch((error) => console.log(error));
