6a17bcf51416c096c3621ac9dfc36a97bd9849fc
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-27 10:29:10 +0530

    solving

--- javaScript/project-Learning/inventory-project/app.js
+++ javaScript/project-Learning/inventory-project/app.js
@@ -1,71 +1,14 @@
 const products = [
-  {
-    id: 1,
-    name: "Keyboard",
-    category: "Electronics",
-    price: 1200,
-    stock: 15,
-  },
-  {
-    id: 2,
-    name: "Mouse",
-    category: "Electronics",
-    price: 500,
-    stock: 30,
-  },
-  {
-    id: 3,
-    name: "Monitor",
-    category: "Electronics",
-    price: 8000,
-    stock: 8,
-  },
-  { id: 4, name: "Notebook", category: "Stationery", price: 80, stock: 50 },
-  { id: 5, name: "USB Cable", category: "Electronics", price: 250, stock: 3 },
+  { id: 1, name: "Laptop", stock: 12, price: 65000 },
+  { id: 2, name: "Mouse", stock: 0, price: 800 },
+  { id: 3, name: "Keyboard", stock: 5, price: 1500 },
+  { id: 4, name: "Monitor", stock: 0, price: 12000 },
 ];
 
-function addProduct(id, name, category, price, stock) {
-  const newProduct = {
-    id,
-    name,
-    category,
-    price,
-    stock,
-  };
-  products.push(newProduct);
+function getOutOfStockProducts(products) {
+  const outOfStock = products.filter((product) => product.stock === 0);
+  return outOfStock;
 }
 
-addProduct(6, "Headphones", "Electronics", 2500, 12);
-
-function showProducts() {
-  products.forEach((product) => {
-    console.log(`${product.id} - ${product.name}`);
-  });
-}
-
-function findProduct(inputId) {
-  const matchedId = products.find((product) => product.id === inputId);
-  const { name, price, stock } = matchedId;
-  if (!matchedId) {
-    console.log("No product found");
-  } else {
-    console.log(name);
-    console.log(`₹${price}`);
-    console.log(`Stock:${stock}`);
-  }
-  return matchedId;
-}
-
-function updateStock(id, newStock) {
-  const matchedId = products.find((product) => product.id === id);
-  matchedId.stock = newStock;
-}
-
-function deleteProduct(id) {
-  const updatedProduct = products.filter((product) => product.id !== id);
-  updatedProduct.forEach((product) => {
-    console.log(`${product.id} - ${product.name}`);
-  });
-}
-
-deleteProduct(2);
+const result = getOutOfStockProducts(products);
+console.log(result);
