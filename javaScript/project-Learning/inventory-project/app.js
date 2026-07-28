027f46a6cf6d6b04c1d37e0d6d8886514d4443ae
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-28 13:58:54 +0530

    solving

--- javaScript/project-Learning/inventory-project/app.js
+++ javaScript/project-Learning/inventory-project/app.js
@@ -1,3 +1,18 @@
+// const products = [
+//   { id: 1, name: "Laptop", stock: 12, price: 65000 },
+//   { id: 2, name: "Mouse", stock: 0, price: 800 },
+//   { id: 3, name: "Keyboard", stock: 5, price: 1500 },
+//   { id: 4, name: "Monitor", stock: 0, price: 12000 },
+// ];
+
+// function getOutOfStockProducts(products) {
+//   const outOfStock = products.filter((product) => product.stock === 0);
+//   return outOfStock;
+// }
+
+// const result = getOutOfStockProducts(products);
+// console.log(result);
+
 const products = [
   { id: 1, name: "Laptop", stock: 12, price: 65000 },
   { id: 2, name: "Mouse", stock: 0, price: 800 },
@@ -5,10 +20,4 @@ const products = [
   { id: 4, name: "Monitor", stock: 0, price: 12000 },
 ];
 
-function getOutOfStockProducts(products) {
-  const outOfStock = products.filter((product) => product.stock === 0);
-  return outOfStock;
-}
-
-const result = getOutOfStockProducts(products);
-console.log(result);
+function getMostExpensiveProduct(products) {}
