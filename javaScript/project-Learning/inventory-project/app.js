94aa96de6333ef19f72a8d507236f257ef3ec58f
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-31 11:47:58 +0530

    solving

--- javaScript/project-Learning/inventory-project/app.js
+++ javaScript/project-Learning/inventory-project/app.js
@@ -1,18 +1,3 @@
-// const products = [
-//   { id: 1, name: "Laptop", stock: 12, price: 65000 },
-//   { id: 2, name: "Mouse", stock: 0, price: 800 },
-//   { id: 3, name: "Keyboard", stock: 5, price: 1500 },
-//   { id: 4, name: "Monitor", stock: 0, price: 12000 },
-// ];
-
-// function getOutOfStockProducts(products) {
-//   const outOfStock = products.filter((product) => product.stock === 0);
-//   return outOfStock;
-// }
-
-// const result = getOutOfStockProducts(products);
-// console.log(result);
-
 const products = [
   { id: 1, name: "Laptop", stock: 12, price: 65000 },
   { id: 2, name: "Mouse", stock: 0, price: 800 },
@@ -20,4 +5,9 @@ const products = [
   { id: 4, name: "Monitor", stock: 0, price: 12000 },
 ];
 
-function getMostExpensiveProduct(products) {}
+function updateProductPrice(products, proId, newPrice) {
+  const matchedProduct = products.filter((product) => product.id === proId);
+  return matchedProduct.map((product) => (product.price = newPrice));
+}
+
+console.log(updateProductPrice(products, 3, 2000));
