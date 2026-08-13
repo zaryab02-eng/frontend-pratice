45545f2dbefe58788b8bbc460664168a635ae858
zaryab02-eng <mohdzaryab99@gmail.com>
2026-08-13 15:13:20 +0530

    solving

--- javaScript/project-Learning/inventory-project/app.js
+++ javaScript/project-Learning/inventory-project/app.js
@@ -2,13 +2,4 @@ const products = [
   { id: 1, name: "Laptop", stock: 12, price: 65000 },
   { id: 2, name: "Mouse", stock: 0, price: 800 },
   { id: 3, name: "Keyboard", stock: 5, price: 1500 },
-  { id: 4, name: "Monitor", stock: 0, price: 12000 },
 ];
-
-function updateProductPrice(products, proId, newPrice) {
-  return products.map((product) => {
-    return product.id === proId ? (product.price = newPrice) : product.price;
-  });
-}
-
-console.log(updateProductPrice(products, 3, 2000));
