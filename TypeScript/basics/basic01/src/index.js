5d7561db670ca13b45790b667e418a351741d147
zaryab02-eng <mohdzaryab99@gmail.com>
2026-08-25 13:14:42 +0530

    learning

--- TypeScript/basics/basic01/src/index.js
+++ TypeScript/basics/basic01/src/index.js
@@ -1,13 +1,20 @@
 "use strict";
+// function formatId(id: string | number): string {
+//   if (typeof id === "string") {
+//     return `ID:${id}`;
+//   } else {
+//     return `ID:${id}`;
+//   }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-function formatId(id) {
-    if (typeof id === "string") {
-        return `ID:${id}`;
+function getRole(person) {
+    if ("salary" in person) {
+        return "Employee";
     }
     else {
-        return `ID:${id}`;
+        return "Manager";
     }
 }
-console.log(formatId(101));
-console.log(formatId("EMP-101"));
+console.log(getRole({ name: "Zaryab", teamSize: 5 }));
+console.log(getRole({ name: "Ali", salary: 4500 }));
 //# sourceMappingURL=index.js.map
--- TypeScript/basics/basic01/src/index.ts
+++ TypeScript/basics/basic01/src/index.ts
@@ -1,11 +1,32 @@
-function formatId(id: string | number): string {
-  if (typeof id === "string") {
-    return `ID:${id}`;
+// function formatId(id: string | number): string {
+//   if (typeof id === "string") {
+//     return `ID:${id}`;
+//   } else {
+//     return `ID:${id}`;
+//   }
+// }
+
+// console.log(formatId(101));
+
+// console.log(formatId("EMP-101"));
+
+interface Employee {
+  name: string;
+  salary: number;
+}
+
+interface Manager {
+  name: string;
+  teamSize: number;
+}
+
+function getRole(person: Employee | Manager): string {
+  if ("salary" in person) {
+    return "Employee";
   } else {
-    return `ID:${id}`;
+    return "Manager";
   }
 }
 
-console.log(formatId(101));
-
-console.log(formatId("EMP-101"));
+console.log(getRole({ name: "Zaryab", teamSize: 5 }));
+console.log(getRole({ name: "Ali", salary: 4500 }));
