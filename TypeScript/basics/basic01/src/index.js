9cc33ac4c84138a1854e35d7c2f6091ef94086ad
zaryab02-eng <mohdzaryab99@gmail.com>
2026-08-27 23:39:53 +0530

    working

--- TypeScript/basics/basic01/src/index.js
+++ TypeScript/basics/basic01/src/index.js
@@ -1,13 +1,39 @@
 "use strict";
+// type Payment =
+//   | { status: "success"; amount: number }
+//   | { status: "failed"; reason: string };
 Object.defineProperty(exports, "__esModule", { value: true });
-function getPaymentMessage(payment) {
-    if (payment.status === "success") {
-        return `Payment successful: ₹${payment.amount}`;
-    }
-    else {
-        return `Payment failed: ${payment.reason}`;
-    }
+// function getPaymentMessage(payment: Payment): string {
+//   if (payment.status === "success") {
+//     return `Payment successful: ₹${payment.amount}`;
+//   } else {
+//     return `Payment failed: ${payment.reason}`;
+//   }
+// }
+// console.log(getPaymentMessage({ status: "success", amount: 500 }));
+// console.log(
+//   getPaymentMessage({ status: "failed", reason: "Insufficient balance" }),
+// );
+// interface Admin {
+//   name: string;
+//   permissions: string[];
+// }
+// interface Customer {
+//   name: string;
+//   order: number;
+// }
+// function isAdmin(user: Admin | Customer): boolean {
+//   if ("permissions" in user) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(isAdmin({ name: "zaryab", order: 2 }));
+// const productInfo: [string, number, boolean] = ["keyboard", 2500, true];
+// console.log(productInfo);
+function getProduct() {
+    return ["mouse", 200, true];
 }
-console.log(getPaymentMessage({ status: "success", amount: 500 }));
-console.log(getPaymentMessage({ status: "failed", reason: "Insufficient balance" }));
+console.log(getProduct());
 //# sourceMappingURL=index.js.map
--- TypeScript/basics/basic01/src/index.ts
+++ TypeScript/basics/basic01/src/index.ts
@@ -1,16 +1,44 @@
-type Payment =
-  | { status: "success"; amount: number }
-  | { status: "failed"; reason: string };
+// type Payment =
+//   | { status: "success"; amount: number }
+//   | { status: "failed"; reason: string };
 
-function getPaymentMessage(payment: Payment): string {
-  if (payment.status === "success") {
-    return `Payment successful: ₹${payment.amount}`;
-  } else {
-    return `Payment failed: ${payment.reason}`;
-  }
+// function getPaymentMessage(payment: Payment): string {
+//   if (payment.status === "success") {
+//     return `Payment successful: ₹${payment.amount}`;
+//   } else {
+//     return `Payment failed: ${payment.reason}`;
+//   }
+// }
+
+// console.log(getPaymentMessage({ status: "success", amount: 500 }));
+// console.log(
+//   getPaymentMessage({ status: "failed", reason: "Insufficient balance" }),
+// );
+
+// interface Admin {
+//   name: string;
+//   permissions: string[];
+// }
+
+// interface Customer {
+//   name: string;
+//   order: number;
+// }
+
+// function isAdmin(user: Admin | Customer): boolean {
+//   if ("permissions" in user) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isAdmin({ name: "zaryab", order: 2 }));
+
+// const productInfo: [string, number, boolean] = ["keyboard", 2500, true];
+// console.log(productInfo);
+
+function getProduct(): [string, number, boolean] {
+  return ["mouse", 200, true];
 }
-
-console.log(getPaymentMessage({ status: "success", amount: 500 }));
-console.log(
-  getPaymentMessage({ status: "failed", reason: "Insufficient balance" }),
-);
+console.log(getProduct());
