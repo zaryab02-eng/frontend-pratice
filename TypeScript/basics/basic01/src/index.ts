3b467a2d10a0fc2eb488c180a9a5fb63bd4f5cb4
zaryab02-eng <mohdzaryab99@gmail.com>
2026-09-01 00:24:58 +0530

    lwarning

--- TypeScript/basics/basic01/src/index.ts
+++ TypeScript/basics/basic01/src/index.ts
@@ -1,44 +1,25 @@
-// type Payment =
-//   | { status: "success"; amount: number }
-//   | { status: "failed"; reason: string };
-
-// function getPaymentMessage(payment: Payment): string {
-//   if (payment.status === "success") {
-//     return `Payment successful: ₹${payment.amount}`;
-//   } else {
-//     return `Payment failed: ${payment.reason}`;
-//   }
-// }
-
-// console.log(getPaymentMessage({ status: "success", amount: 500 }));
-// console.log(
-//   getPaymentMessage({ status: "failed", reason: "Insufficient balance" }),
-// );
-
-// interface Admin {
-//   name: string;
-//   permissions: string[];
-// }
-
-// interface Customer {
-//   name: string;
-//   order: number;
-// }
-
-// function isAdmin(user: Admin | Customer): boolean {
-//   if ("permissions" in user) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// console.log(isAdmin({ name: "zaryab", order: 2 }));
-
-// const productInfo: [string, number, boolean] = ["keyboard", 2500, true];
-// console.log(productInfo);
-
-function getProduct(): [string, number, boolean] {
-  return ["mouse", 200, true];
+interface Product {
+  id: number;
+  name: string;
+  price: number;
 }
-console.log(getProduct());
+
+type ProductStatus = "Available" | "Sold";
+
+interface InventoryItem {
+  product: Product;
+  status: ProductStatus;
+}
+
+function proInventory(): InventoryItem {
+  return {
+    product: {
+      id: 101,
+      name: "parle",
+      price: 50,
+    },
+    status: "Available",
+  };
+}
+
+console.log(proInventory());
