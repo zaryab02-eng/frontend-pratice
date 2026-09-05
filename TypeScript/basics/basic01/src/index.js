6174c952c537ff5fd5282c8ec6653e0c0cff4a88
zaryab02-eng <mohdzaryab99@gmail.com>
2026-09-05 14:58:11 +0530

    trying to code

--- TypeScript/basics/basic01/src/index.js
+++ TypeScript/basics/basic01/src/index.js
@@ -1,14 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function proInventory() {
-    return {
-        product: {
-            id: 101,
-            name: "parle",
-            price: 50,
-        },
-        status: "Available",
-    };
+const products = [
+    { id: 1, name: "Keyboard", price: 2500, inStock: true },
+    { id: 2, name: "Mouse", price: 1200, inStock: false },
+    { id: 3, name: "Monitor", price: 15000, inStock: true },
+];
+function updateStock(id, status) {
+    return products.map((product) => {
+        if (product.id === id) {
+            return {
+                ...product,
+                inStock: status,
+            };
+        }
+        return product;
+    });
 }
-console.log(proInventory());
+console.log(updateStock(2, true));
 //# sourceMappingURL=index.js.map
--- TypeScript/basics/basic01/src/index.ts
+++ TypeScript/basics/basic01/src/index.ts
@@ -2,24 +2,25 @@ interface Product {
   id: number;
   name: string;
   price: number;
+  inStock: boolean;
 }
 
-type ProductStatus = "Available" | "Sold";
+const products: Product[] = [
+  { id: 1, name: "Keyboard", price: 2500, inStock: true },
+  { id: 2, name: "Mouse", price: 1200, inStock: false },
+  { id: 3, name: "Monitor", price: 15000, inStock: true },
+];
 
-interface InventoryItem {
-  product: Product;
-  status: ProductStatus;
+function updateStock(id: number, status: boolean): Product[] {
+  return products.map((product) => {
+    if (product.id === id) {
+      return {
+        ...product,
+        inStock: status,
+      };
+    }
+    return product;
+  });
 }
 
-function proInventory(): InventoryItem {
-  return {
-    product: {
-      id: 101,
-      name: "parle",
-      price: 50,
-    },
-    status: "Available",
-  };
-}
-
-console.log(proInventory());
+console.log(updateStock(2, true));
