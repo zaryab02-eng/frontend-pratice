951f166071b805d653ead3b7ae91c6b95e8fbeb0
zaryab02-eng <mohdzaryab99@gmail.com>
2026-09-19 11:32:46 +0530

    ehehehehehehehehh yayayyayaya wwawawawa

--- TypeScript/basics/basic01/src/index.js
+++ TypeScript/basics/basic01/src/index.js
@@ -5,4 +5,10 @@ const products = [
     { id: 2, name: "Mouse", price: 1200, inStock: false },
     { id: 3, name: "Monitor", price: 15000, inStock: true },
 ];
+function getTotalPrice() {
+    return products.reduce((sum, product) => {
+        return sum + product.price;
+    }, 0);
+}
+console.log(getTotalPrice());
 //# sourceMappingURL=index.js.map
--- TypeScript/basics/basic01/src/index.ts
+++ TypeScript/basics/basic01/src/index.ts
@@ -10,3 +10,11 @@ const products: Product[] = [
   { id: 2, name: "Mouse", price: 1200, inStock: false },
   { id: 3, name: "Monitor", price: 15000, inStock: true },
 ];
+
+function getTotalPrice(): number {
+  return products.reduce((sum, product) => {
+    return sum + product.price;
+  }, 0);
+}
+
+console.log(getTotalPrice());
